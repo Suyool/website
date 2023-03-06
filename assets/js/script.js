7e0c88d4c0149225dd1ee94f8e5ed7ad90c786e5
elio najem <elio.najem@elbarid.com>
2023-03-06 13:03:50 +0200

    fix the js eventlistener conflict

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,3 @@
-// Get the query string from the current URL
-const queryString = window.location.search;
-// Create a new URLSearchParams object from the query string
-const queryParams = new URLSearchParams(queryString);
-
-// Get the value of the "uniqueCode" parameter
-const uniqueCode = queryParams.get("uniqueCode");
-
-// Get the value of the "Flag" parameter
-const flag = queryParams.get("Flag");
-
-
-// Call the resubscribe function with the uniqueCode and flag values
-let btn = document.getElementById("resubscribe");
-btn.addEventListener('click', event => {
-    resubscribe(uniqueCode,flag);
-});
-
 function resubscribe(uniqueCode, flag) {
     jQuery.ajax({
         type: "GET",
@@ -23,5 +5,4 @@ function resubscribe(uniqueCode, flag) {
         dataType: 'json',
     });
 }
-
 
