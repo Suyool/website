88dc6d8bdd43d8299a1cd6be09bb6641892f912a
YAMMOUNII <elie.yammouny@hotmail.com>
2023-06-08 12:14:37 +0300

    implemennt react in symfony

---
+++ assets/react/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = ({ v }) => {
+    // const [GetDisplay,]
+
+    return (
+        <div>
+            Hello Elie! The value of "v" is: {v}
+        </div>
+    );
+};
+
+export default App;
