6a756190e044fbaa08021a4e30939c51846d0fc8
YAMMOUNII <elie.yammouny@hotmail.com>
2023-06-08 13:45:47 +0300

    preDone with subNavigation

--- assets/react/components/BottomNav.js
+++ assets/react/components/BottomNav.js
@@ -1,28 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
-const App = ({ v }) => {
-    const [activeButton, setActiveButton] = useState({ name: "LLDJ" });
+const BottomNav = ({ activeButton, setActiveButton }) => {
+
     const handleButtonClick = (name) => {
         setActiveButton({ name: name });
     };
-
     return (
-        <div id="LotoBody">
-            <div>
-                {activeButton.name === "LLDJ" && "LLDJ"}
-                {activeButton.name === "Play" && "Play"}
-                {activeButton.name === "Result" && "Result"}
-            </div>
-
-            <div className="navigation">
+        <div className="navigation">
+            <div className="subCont">
                 <button className={`NavBtn ${activeButton.name === "LLDJ" ? "active" : ""}`} onClick={() => { handleButtonClick("LLDJ") }}>
                     <img src={activeButton.name === "LLDJ" ? "/build/images/Loto/LLDJSlected.png" : "/build/images/Loto/LLDJ.png"} alt="LLDJ" />
                     <div className={`title ${activeButton.name === "LLDJ" ? "activeTitle" : ""}`}>LLDJ</div>
                 </button>
-                <button className={`NavBtn ${activeButton.name === "Play" ? "PlayActive" : ""}`} onClick={() => { handleButtonClick("Play") }}>
+                <button className={` ${activeButton.name === "Play" ? "PlayActive" : "NavBtnPlay"}`} onClick={() => { handleButtonClick("Play") }}>
                     <img src={activeButton.name === "Play" ? "/build/images/Loto/playSelected.png" : "/build/images/Loto/play.png"} alt="Play" />
                     <div className={`title ${activeButton.name === "Play" ? "activeTitle" : ""}`}>{activeButton.name === "Play" ? "" : "Play"}</div>
                 </button>
+                <div></div>
                 <button className={`NavBtn ${activeButton.name === "Result" ? "active" : ""}`} onClick={() => { handleButtonClick("Result") }}>
                     <img src={activeButton.name === "Result" ? "/build/images/Loto/resultSelected.png" : "/build/images/Loto/result.png"} alt="Result" />
                     <div className={`title ${activeButton.name === "Result" ? "activeTitle" : ""}`}>Result</div>
@@ -32,4 +26,4 @@ const App = ({ v }) => {
     );
 };
 
-export default App;
+export default BottomNav;
