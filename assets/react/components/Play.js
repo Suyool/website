8a27c00fac854fa6ca62ff1ee07106761e7901a0
YAMMOUNII <elie.yammouny@hotmail.com>
2023-06-09 11:58:24 +0300

    pre done with play page design

--- assets/react/components/Play.js
+++ assets/react/components/Play.js
@@ -5,27 +5,85 @@ const Play = () => {
     return (
         <div id="Play">
             <h3 className="gridplays">How many lottery grids do you want to play?</h3>
+
             <div className="gridborder">
                 <div className="header">
-                    <span><img src="/build/images/Loto/LotoGrid.png" alt="loto"/>GRID 1</span>
+                    <span><img src="/build/images/Loto/LotoGrid.png" alt="loto" />GRID 1</span>
                     <span className="right">PLAY ZEED (+ L.L 5,000 ) CHECKBOX</span>
                 </div>
                 <div className="body">
-                <div className="ballSection mt-2">
-                    <span>11</span>
-                    <span>16</span>
-                    <span>18</span>
-                    <span>27</span>
-                    <span>29</span>
-                    <span>42</span>
-                </div>
-                <div className="edit"><img src="/build/images/Loto/edit.png"/></div>
+                    <div className="ballSection mt-2">
+                        <span>11</span>
+                        <span>16</span>
+                        <span>18</span>
+                        <span>27</span>
+                        <span>29</span>
+                        <span>42</span>
+                    </div>
+                    <div className="edit"><img src="/build/images/Loto/edit.png" /></div>
                 </div>
                 <div className="footer">
                     <span className="price">L.L 200,000</span>
-                    <span className="delete"><img src="/build/images/Loto/trash.png"/></span>
+                    <span className="delete"><img src="/build/images/Loto/trash.png" /></span>
                 </div>
             </div>
+
+            <div className="addGrid">
+                <span>+</span>
+            </div>
+
+            <div className="wantToPlay">
+                <div className="title">How often do you want to play?</div>
+
+                <div className="listSection">
+
+                    <div className="listItem">
+                        <div className="checkbox">true</div>
+                        <div className="playNB">
+                            <div className="titleNb">Play Once</div>
+                            <div className="desc">Thursday X at 9:00PM</div>
+                        </div>
+                        <div className="price">200,000 LBP</div>
+                    </div>
+
+                    <div className="listItem">
+                        <div className="checkbox">true</div>
+                        <div className="playNB">
+                            <div className="titleNb">Play Once</div>
+                            <div className="desc">Thursday X at 9:00PM</div>
+                        </div>
+                        <div className="price">200,000 LBP</div>
+                    </div>
+
+                    <div className="listItem">
+                        <div className="checkbox">true</div>
+                        <div className="playNB">
+                            <div className="titleNb">Play Once</div>
+                            <div className="desc">Thursday X at 9:00PM</div>
+                        </div>
+                        <div className="price">200,000 LBP</div>
+                    </div>
+
+                    <div className="listItem">
+                        <div className="checkbox">true</div>
+                        <div className="playNB">
+                            <div className="titleNb">Play Once</div>
+                            <div className="desc">Thursday X at 9:00PM</div>
+                        </div>
+                        <div className="price">200,000 LBP</div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="btnSection">
+                <div className="Total">
+                    <span>TOTAL</span>
+                    <span>L.L 200,000</span>
+                </div>
+                <button>Checkout</button>
+            </div>
+
         </div>
     );
 };
--- src/Controller/LotoController.php
+++ src/Controller/LotoController.php
@@ -31,14 +31,16 @@ class LotoController extends AbstractController
         $NextDraw = json_decode($NextDrawresponse, true);
 
         // dd($NextDraw);
+        if ($NextDraw) {
+            $parameters['next_draw_number'] = $NextDraw['d']['draws'][0]['drawnumber'];
+            $parameters['next_loto_win'] = $NextDraw['d']['draws'][1]['lotojackpotLBP'];
+            $parameters['next_zeed_win'] = $NextDraw['d']['draws'][1]['zeedjackpotLBP'];
+            $parameters['next_date'] = $NextDraw['d']['draws'][0]['drawdate'];
+        }
 
-        $parameters['next_draw_number'] = $NextDraw['d']['draws'][0]['drawnumber'];
-        $parameters['next_loto_win'] = $NextDraw['d']['draws'][1]['lotojackpotLBP'];
-        $parameters['next_zeed_win'] = $NextDraw['d']['draws'][1]['zeedjackpotLBP'];
-        $parameters['next_date'] = $NextDraw['d']['draws'][0]['drawdate'];
 
         $next_date = new DateTime($parameters['next_date']);
-        $interval=new DateInterval('PT3H');
+        $interval = new DateInterval('PT3H');
         $next_date->add($interval);
         $parameters['next_date'] = $next_date->format('l, M d Y H:i:s');
 
@@ -49,7 +51,7 @@ class LotoController extends AbstractController
 
         $gridprice = json_decode($gridpriceresponse, true);
         // dd($gridprice);
-        $onegridprice = (int)$gridprice['d']['stringvalue1'];
+        $onegridprice = (int) $gridprice['d']['stringvalue1'];
         $parameters['Zeedgridprice'] = $gridprice['d']['stringvalue2'];
         $parameters['gridprice'] = [
             'grid1' => $onegridprice,
@@ -70,57 +72,57 @@ class LotoController extends AbstractController
         $numbers = 6;
         $pricematrixarray = [];
         foreach ($GetFullGridPriceMatrix['d']['pricematrix'] as $pricematrix) {
-            if($numbers<11){
-            $pricematrixarray[] = [
-                'numbers' => $numbers,
-                'price' => $pricematrix['price0J'],
-            ];
-        }
+            if ($numbers < 11) {
+                $pricematrixarray[] = [
+                    'numbers' => $numbers,
+                    'price' => $pricematrix['price0J'],
+                ];
+            }
             $numbers++;
         }
 
-        $drawnumber=$parameters['next_draw_number'];
-        $gridselected="2 3 4 5 6 7 1 | 10 22 39 44 33 11 23";
+        $drawnumber = $parameters['next_draw_number'];
+        $gridselected = "2 3 4 5 6 7 1 | 10 22 39 44 33 11 23";
 
-        $submit_loto_play=[
-            'Token'=>'30b7ea61-5319-4dc3-81a2-bf339e625a1e',
-            'drawNumber'=>$drawnumber,
-            'numDraws'=>$drawnumber,
-            'withZeed'=>0,//1 if with zeed
-            'saveToFavorite'=>0,//1 TO ADD TO FAVORITE,
-            'GridsSelected'=>$gridselected,// separated with |
+        $submit_loto_play = [
+            'Token' => '30b7ea61-5319-4dc3-81a2-bf339e625a1e',
+            'drawNumber' => $drawnumber,
+            'numDraws' => $drawnumber,
+            'withZeed' => 0, //1 if with zeed
+            'saveToFavorite' => 0, //1 TO ADD TO FAVORITE,
+            'GridsSelected' => $gridselected, // separated with |
         ];
 
-        $submitloto['data']=json_encode($submit_loto_play);
+        $submitloto['data'] = json_encode($submit_loto_play);
         $submitloto['url'] = "/Servicev2.asmx/SubmitLotoPlayOrder";
         $ResponseGetFullGridPriceMatrix = Helper::send_curl($submitloto);
 
-        $submit_loto=json_decode($ResponseGetFullGridPriceMatrix,true);
-        if($submit_loto['d']['errorinfo']['errorcode']>0){
-            $parameters['submit_loto']=$submit_loto['d']['errorinfo'];
-        }else{
-            $parameters['submit_loto']=['insertId'=>$submit_loto['d']['insertId'],'balance'=>$submit_loto['d']['balance'],'token'=>$submit_loto['d']['token']];
+        $submit_loto = json_decode($ResponseGetFullGridPriceMatrix, true);
+        if ($submit_loto['d']['errorinfo']['errorcode'] > 0) {
+            $parameters['submit_loto'] = $submit_loto['d']['errorinfo'];
+        } else {
+            $parameters['submit_loto'] = ['insertId' => $submit_loto['d']['insertId'], 'balance' => $submit_loto['d']['balance'], 'token' => $submit_loto['d']['token']];
         }
         // dd($submit_loto);
         // $parameters['matrix'] = $pricematrixarray;
-        $gridnumber="2 3 4 5 6 7 1";
-        $grid_price_by_number=[
-            'Token'=>'30b7ea61-5319-4dc3-81a2-bf339e625a1e',
-            'Grid'=>$gridnumber
+        $gridnumber = "2 3 4 5 6 7 1";
+        $grid_price_by_number = [
+            'Token' => '30b7ea61-5319-4dc3-81a2-bf339e625a1e',
+            'Grid' => $gridnumber
         ];
 
-        $gridpricebynum['data']=json_encode($grid_price_by_number);
+        $gridpricebynum['data'] = json_encode($grid_price_by_number);
         $gridpricebynum['url'] = "/Servicev2.asmx/GetGridPrice";
         $gridpricebynumResponse = Helper::send_curl($gridpricebynum);
-        
-        $gridpricebynumData=json_decode($gridpricebynumResponse,true);
-        $parameters['gridpricebynum']=$gridpricebynumData['d'];
-        $parameters['gridpricematrix']=$pricematrixarray;
+
+        $gridpricebynumData = json_decode($gridpricebynumResponse, true);
+        $parameters['gridpricebynum'] = $gridpricebynumData['d'];
+        $parameters['gridpricematrix'] = $pricematrixarray;
 
 
         // dd($parameters);
-        return $this->render('loto/index.html.twig',[
-            'parameters'=>$parameters
+        return $this->render('loto/index.html.twig', [
+            'parameters' => $parameters
         ]);
     }
 }
