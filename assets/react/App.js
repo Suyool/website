2c2e5557ff14aff93239eaf7287b7fd8c2b3390a
YAMMOUNII <elie.yammouny@hotmail.com>
2023-06-09 13:26:50 +0300

    working on pick your grid

--- assets/react/App.js
+++ assets/react/App.js
@@ -3,9 +3,11 @@ import BottomNav from "./components/BottomNav";
 import LLDJ from "./components/LLDJ";
 import Play from "./components/Play";
 import Result from "./components/Result.js";
+import PickYourGrid from "./components/PickYourGrid";
 
 const App = ({ parameters }) => {
-    const [activeButton, setActiveButton] = useState({ name: "Play" });
+    const [activeButton, setActiveButton] = useState({ name: "LLDJ" });
+    const [getPickYourGrid, setPickYourGrid] = useState(true);
 
     return (
         <div id="LotoBody">
@@ -18,6 +20,7 @@ const App = ({ parameters }) => {
                 {activeButton.name === "Result" && <Result />}
             </div>
 
+            {getPickYourGrid && <PickYourGrid />}
             <BottomNav activeButton={activeButton} setActiveButton={setActiveButton} />
         </div>
     );
---
+++ assets/react/components/PickYourGrid.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+const PickYourGrid = () => {
+
+    return (
+        <div className="PickYourGrid">
+            <div className="topSectionPick">
+                <div className="titles">
+                    <div className="titleGrid">Pick Your Grid</div>
+                    <button>Cancel</button>
+                </div>
+
+                <div className="selectedBalls">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+            </div>
+            <div className="bodySectionPick">
+                <div className="ballCont"><span>1</span></div>
+                <div className="ballCont"><span>2</span></div>
+                <div className="ballCont"><span>3</span></div>
+                <div className="ballCont"><span>4</span></div>
+                <div className="ballCont"><span>5</span></div>
+                <div className="ballCont"><span>6</span></div>
+                <div className="ballCont"><span>7</span></div>
+                <div className="ballCont"><span>8</span></div>
+                <div className="ballCont"><span>9</span></div>
+                <div className="ballCont"><span>10</span></div>
+                <div className="ballCont"><span>11</span></div>
+                <div className="ballCont"><span>12</span></div>
+                <div className="ballCont"><span>13</span></div>
+                <div className="ballCont"><span>14</span></div>
+                <div className="ballCont"><span>15</span></div>
+                <div className="ballCont"><span>16</span></div>
+                <div className="ballCont"><span>17</span></div>
+                <div className="ballCont"><span>18</span></div>
+                <div className="ballCont"><span>19</span></div>
+                <div className="ballCont"><span>20</span></div>
+                <div className="ballCont"><span>21</span></div>
+                <div className="ballCont"><span>22</span></div>
+                <div className="ballCont"><span>23</span></div>
+                <div className="ballCont"><span>24</span></div>
+                <div className="ballCont"><span>25</span></div>
+                <div className="ballCont"><span>26</span></div>
+                <div className="ballCont"><span>27</span></div>
+                <div className="ballCont"><span>28</span></div>
+                <div className="ballCont"><span>29</span></div>
+                <div className="ballCont"><span>30</span></div>
+                <div className="ballCont"><span>31</span></div>
+                <div className="ballCont"><span>32</span></div>
+                <div className="ballCont"><span>33</span></div>
+                <div className="ballCont"><span>34</span></div>
+                <div className="ballCont"><span>35</span></div>
+                <div className="ballCont"><span>36</span></div>
+                <div className="ballCont"><span>37</span></div>
+                <div className="ballCont"><span>38</span></div>
+                <div className="ballCont"><span>39</span></div>
+                <div className="ballCont"><span>40</span></div>
+                <div className="ballCont"><span>41</span></div>
+                <div className="ballCont"><span>42</span></div>
+            </div>
+            <div className="footSectionPick">
+                <div className="Total">
+                    <span>TOTAL</span>
+                    <span>L.L 200,000</span>
+                </div>
+
+                <div className="options">
+                    <button className="aboutGrid">Clear grid</button>
+                    <button className="aboutGrid">Quick pick</button>
+                    <button className="done">Done</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PickYourGrid;
