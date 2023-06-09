ddf3398a21478c314fa8ea470cc445926f890328
YAMMOUNII <elie.yammouny@hotmail.com>
2023-06-09 09:51:15 +0300

    working on result

--- assets/react/components/Result.js.js
+++ assets/react/components/Result.js.js
@@ -4,7 +4,49 @@ const Result = () => {
 
     return (
         <div id="Result">
+            <div className="resultTopSection mt-4">
+                <div className="title">Draw Numbers</div>
+                <div className="ballSection mt-2">
+                    <span>11</span>
+                    <span>16</span>
+                    <span>18</span>
+                    <span>27</span>
+                    <span>29</span>
+                    <span>42</span>
+                </div>
+            </div>
 
+            <div className="nextDrawSection mt-4">
+                <div className="selectTime">May 2023</div>
+                
+                <div className="dayDrow">
+                    <div className="goNext"><img src="/build/images/Loto/goNext.png" alt="goNext" /></div>
+                    <div className="items">
+                        <div className="item">
+                            <div className="time">14</div>
+                            <div className="day">Mon</div>
+                        </div>
+                        <div className="item">
+                            <div className="time">14</div>
+                            <div className="day">Mon</div>
+                        </div>
+                        <div className="item">
+                            <div className="time">14</div>
+                            <div className="day">Mon</div>
+                        </div>
+                        <div className="item">
+                            <div className="time">14</div>
+                            <div className="day">Mon</div>
+                        </div>
+                        <div className="item">
+                            <div className="time">14</div>
+                            <div className="day">Mon</div>
+                        </div>
+                    </div>
+                    <div className="goNext"><img src="/build/images/Loto/goNext.png" alt="goNext" /></div>
+                </div>
+
+            </div>
         </div>
     );
 };
