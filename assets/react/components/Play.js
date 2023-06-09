ffac64b4591459858cc88544bfe7f115cb2015a4
Anthony Saliba <anthony.saliban@outlook.com>
2023-06-09 09:39:01 +0300

    play one section

--- assets/react/components/Play.js
+++ assets/react/components/Play.js
@@ -7,7 +7,23 @@ const Play = () => {
             <h3 className="gridplays">How many lottery grids do you want to play?</h3>
             <div className="gridborder">
                 <div className="header">
-                    <span><img src="/build/images/Loto/LibanaiseJeux.png" alt="loto"/>GRID 1</span>
+                    <span><img src="/build/images/Loto/LotoGrid.png" alt="loto"/>GRID 1</span>
+                    <span className="right">PLAY ZEED (+ L.L 5,000 ) CHECKBOX</span>
+                </div>
+                <div className="body">
+                <div className="ballSection mt-2">
+                    <span>11</span>
+                    <span>16</span>
+                    <span>18</span>
+                    <span>27</span>
+                    <span>29</span>
+                    <span>42</span>
+                </div>
+                <div className="edit"><img src="/build/images/Loto/edit.png"/></div>
+                </div>
+                <div className="footer">
+                    <span className="price">L.L 200,000</span>
+                    <span className="delete"><img src="/build/images/Loto/trash.png"/></span>
                 </div>
             </div>
         </div>
