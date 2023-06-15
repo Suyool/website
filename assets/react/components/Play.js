bfd2874257f15951280bc7c7a0660973941cc135
YAMMOUNII <elie.yammouny@hotmail.com>
2023-06-15 09:34:34 +0300

    fix grid display

--- assets/react/components/Play.js
+++ assets/react/components/Play.js
@@ -1,34 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Play = () => {
+    const [getPlayedBalls, setPlayedBalls] = useState(JSON.parse(localStorage.getItem("selectedBalls")) || []);
+
+    const handleDelete = (index) => {
+        const updatedBalls = [...getPlayedBalls];
+        updatedBalls.splice(index, 1); // Remove the selected balls from the array
+
+        setPlayedBalls(updatedBalls); // Update the state
+
+        // Update the localStorage
+        localStorage.setItem("selectedBalls", JSON.stringify(updatedBalls));
+    };
 
     return (
         <div id="Play">
             <h3 className="gridplays">How many lottery grids do you want to play?</h3>
 
-            <div className="gridborder">
-                <div className="header">
-                    <span><img src="/build/images/Loto/LotoGrid.png" alt="loto" />GRID 1</span>
-                    <span className="right">PLAY ZEED (+ L.L 5,000 )
-                        <input className="switch" type="checkbox" />
-                    </span>
-                </div>
-                <div className="body">
-                    <div className="ballSection mt-2">
-                        <span>11</span>
-                        <span>16</span>
-                        <span>18</span>
-                        <span>27</span>
-                        <span>29</span>
-                        <span>42</span>
+            {getPlayedBalls && getPlayedBalls.map((balls, index) => (
+                <div className="gridborder mt-4" key={index}>
+                    <div className="header">
+                        <span>
+                            <img src="/build/images/Loto/LotoGrid.png" alt="loto" /> GRID {index + 1}
+                        </span>
+                        <span className="right">
+                            PLAY ZEED (+ L.L 5,000)
+                            <input className="switch" type="checkbox" />
+                        </span>
                     </div>
-                    <div className="edit"><img src="/build/images/Loto/edit.png" /></div>
+                    <div className="body">
+                        <div className="ballSection mt-2">
+                            {balls.map((ball, index) => (
+                                <span key={index}>{ball}</span>
+                            ))}
+                        </div>
+                        <div className="edit">
+                            <img src="/build/images/Loto/edit.png" alt="edit" />
+                        </div>
+                    </div>
+                    <div className="footer">
+                        <span className="price">L.L 200,000</span>
+                        <span className="delete" onClick={() => handleDelete(index)}>
+                            <img src="/build/images/Loto/trash.png" alt="delete" />
+                        </span>
+                    </div>
                 </div>
-                <div className="footer">
-                    <span className="price">L.L 200,000</span>
-                    <span className="delete"><img src="/build/images/Loto/trash.png" /></span>
-                </div>
-            </div>
+            ))}
 
             <div className="addGrid">
                 <span>+</span>
