88cd0c578d66a438925c8c3b7528210530a8dd33
Anthony Saliba <anthony.saliban@outlook.com>
2023-06-21 13:44:39 +0300

    submit play loto

---
+++ src/Entity/Loto/LOTO_plays.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity\Loto;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="plays")
+ */
+class LOTO_plays
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * 
+     * @ORM\Column(name="drawNumber",type="string")
+     */
+    private $drawNumber;
+
+     /**
+     * 
+     * @ORM\Column(name="numdraws",type="string")
+     */
+    private $numdraws;
+
+     /**
+     * 
+     * @ORM\Column(name="withZeed")
+     */
+    private $withZeed;
+
+     /**
+     * 
+     * @ORM\Column(name="gridSelected")
+     */
+    private $gridSelected;
+
+    /**
+     * 
+     * @ORM\Column(name="create_date",type="datetime")
+     */
+    private $create_date;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getdrawnumber()
+    {
+        return $this->drawNumber;
+    }
+
+    public function setdrawnumber($drawNumber)
+    {
+        $this->drawNumber=$drawNumber;
+        return $this;
+    }
+
+    public function getnumdraws()
+    {
+        return $this->numdraws;
+    }
+
+    public function setnumdraws($numdraws)
+    {
+        $this->numdraws=$numdraws;
+        return $this;
+    }
+
+    public function getwithZeed()
+    {
+        return $this->withZeed;
+    }
+
+    public function setWithZeed($withZeed)
+    {
+        $this->withZeed=$withZeed;
+        return $this;
+    }
+
+    public function getgridSelected()
+    {
+        return $this->gridSelected;
+    }
+
+    public function setgridSelected($gridSelected)
+    {
+        $this->gridSelected=$gridSelected;
+        return $this;
+    }
+
+
+    public function getcreatedate()
+    {
+        return $this->create_date;
+    }
+
+    public function setcreatedate($create_date)
+    {
+        $this->create_date=$create_date;
+        return $this;
+    }
+
+
+}
