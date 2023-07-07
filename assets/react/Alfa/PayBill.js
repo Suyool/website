9cb832d253b6061ebeb7c484b0f53b4b239682ae
YAMMOUNII <elie.yammouny@hotmail.com>
2023-07-07 08:58:11 +0300

    working on design

--- assets/react/Alfa/PayBill.js
+++ assets/react/Alfa/PayBill.js
@@ -4,7 +4,25 @@ const PayBill = () => {
 
   return (
     <div id="PayBill">
-      PayBill
+      <div className="mainTitle">Enter your phone number to recharge</div>
+
+      <div className="MobileNbContainer mt-3">
+        <div className="place">
+          <img src="/build/images/Alfa/flag.png" alt="flag" />
+          <div className="code">+961</div>
+        </div>
+        <input className="nbInput" placeholder="|" />
+      </div>
+
+      <div className="pCurrency">
+        <div className="subTitle">My Payment Currency</div>
+        <div className="currencies">
+          <div className="activeCurrency">USD</div>
+          <div className="Currency">LBP</div>
+        </div>
+      </div>
+
+      <button id="ContinueBtn" className="btnCont">Continue</button>
     </div>
   );
 };
