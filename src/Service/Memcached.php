b11d0e988769a7b02d3a537d4ffa8a3be3498f66
YAMMOUNII <elie.yammouny@hotmail.com>
2023-07-24 14:28:43 +0300

    add data in a file and read them thrus

--- src/Service/Memcached.php
+++ src/Service/Memcached.php
@@ -2,53 +2,107 @@
 
 namespace App\Service;
 
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\Cache\Adapter\MemcachedAdapter;
 
 class Memcached
 {
-    protected $cache;
-    private $mediumLifetime;
+    // protected $cache;
+    // private $mediumLifetime;
 
-    public function __construct($mediumLifetime)
+    // public function __construct($mediumLifetime)
+    // {
+    //     $this->mediumLifetime = $mediumLifetime;
+
+    //     if (MemcachedAdapter::isSupported()) {
+    //         try {
+    //             $client = MemcachedAdapter::createConnection('memcached://' . "89.108.165.201");
+    //             // dd($client);
+    //             $this->cache = new MemcachedAdapter($client, 'suyool');
+    //         } catch (\ErrorException $e) {;
+    //             dd($e);
+    //         }
+    //     }
+    // }
+    public function __construct()
     {
-        $this->mediumLifetime = $mediumLifetime;
-
-        if (MemcachedAdapter::isSupported()) {
-            try {
-                $client = MemcachedAdapter::createConnection('memcached://' . "89.108.165.201");
-                // dd($client);
-                $this->cache = new MemcachedAdapter($client, 'suyool');
-            } catch (\ErrorException $e) {;
-                dd($e);
-            }
-        }
     }
+
+    // /**
+    //  * @throws \Psr\Cache\InvalidArgumentException
+    //  */
+    // public function testmem($lotoServices)
+    // {
+    //     $filter = $lotoServices->VoucherFilter("ALFA");
+    //     $cacheKey = 'cache_key_for_your_data'; // Choose a unique key for your cache item
+
+    //     if (isset($this->cache)) {
+    //         $cachedData = $this->cache->getItem($cacheKey);
+
+    //         if (!$cachedData->isHit()) {
+    //             $data = $filter;
+    //             $cachedData->set($data)->expiresAfter(3600);
+    //             $this->cache->save($cachedData);
+    //             echo "data from memcached";
+    //         } else {
+    //             echo "data came without memcached without hit";
+    //             $data = $filter;
+    //         }
+    //     } else {
+    //         echo "data came without memcached";
+    //         $data = $filter;
+    //     }
+
+    //     return $data;
+    // }
+
+    // /**
+    //  * @throws \Psr\Cache\InvalidArgumentException
+    //  */
+    // public function testmem($lotoServices)
+    // {
+    //     $filter = $lotoServices->VoucherFilter("ALFA");
+    //     $cacheKey = 'cache_key_for_your_data';
+
+    //     $cachePool = new FilesystemAdapter();
+
+    //     $cachedItem = $cachePool->getItem($cacheKey);
+    //     // dd($cachedItem);
+    //     if (!$cachedItem->isHit()) {
+    //         $data = $filter;
+
+    //         $cachedItem->set($data);
+    //         $cachePool->save($cachedItem);
+    //     } else {
+    //         dd("retrive data");
+    //         $data = $cachedItem->get();
+    //     }
+
+    //     return $data;
+    // }
 
     /**
      * @throws \Psr\Cache\InvalidArgumentException
      */
     public function testmem($lotoServices)
     {
-        $filter = $lotoServices->VoucherFilter("ALFA");
-        $cacheKey = 'cache_key_for_your_data'; // Choose a unique key for your cache item
+        $file = "../var/cache/dev/newfile.txt";
+        $clearingTime = time() - (60);
 
-        if (isset($this->cache)) {
-            $cachedData = $this->cache->getItem($cacheKey);
+        $filter = null;
 
-            if (!$cachedData->isHit()) {
-                $data = $filter;
-                $cachedData->set($data)->expiresAfter($this->mediumLifetime);
-                $this->cache->save($cachedData);
-                echo "data from memcached";
-            } else {
-                echo "data came without memcached without hit";
-                $data = $filter;
-            }
+        if (file_exists($file) && (filemtime($file) > $clearingTime) && (filesize($file) > 0)) {
+            $operationsjson = file_get_contents($file);
+            return json_decode($operationsjson, true);
         } else {
-            echo "data came without memcached";
-            $data = $filter;
+            $filter = $lotoServices->VoucherFilter("ALFA");
+            $jsonData = json_encode($filter);
+
+            $myfile = fopen($file, "w") or die("Unable to open file!");
+            fwrite($myfile, $jsonData);
+            fclose($myfile);
         }
 
-        return $data;
+        return $filter;
     }
 }
