22eb781b06b0a95cb4a1f1a485f52005340636f9
Anthony Saliba <anthony.saliban@outlook.com>
2023-08-08 09:07:56 +0300

    stop the request subscriber

--- src/EventListener/RequestSubscriber.php
+++ src/EventListener/RequestSubscriber.php
@@ -1,37 +1,37 @@
 <?php
 
-namespace App\EventListener;
+// namespace App\EventListener;
 
 
-use Psr\Log\LoggerInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
+// use Psr\Log\LoggerInterface;
+// use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+// use Symfony\Component\HttpKernel\Event\ResponseEvent;
+// use Symfony\Component\HttpKernel\KernelEvents;
 
-class RequestSubscriber implements EventSubscriberInterface
-{
+// class RequestSubscriber implements EventSubscriberInterface
+// {
 
-    private LoggerInterface $logger;
+//     private LoggerInterface $logger;
     
 
 
-    public function __construct(LoggerInterface $logger)
-    {
-        $this->logger=$logger;
-    }
+//     public function __construct(LoggerInterface $logger)
+//     {
+//         $this->logger=$logger;
+//     }
 
-    public static function getSubscribedEvents(): array
-    {
-        return [
-        KernelEvents::RESPONSE => 'onKernelRequest',
-    ];
-    }
+//     public static function getSubscribedEvents(): array
+//     {
+//         return [
+//         KernelEvents::RESPONSE => 'onKernelRequest',
+//     ];
+//     }
 
-    public function onKernelRequest(ResponseEvent $event): void
-    {
-        $this->logger->debug(
-           "successfully"
-        );
+//     public function onKernelRequest(ResponseEvent $event): void
+//     {
+//         $this->logger->debug(
+//            "successfully"
+//         );
 
-    }
-}
+//     }
+// }
