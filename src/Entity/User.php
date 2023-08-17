776864d8b80a971f8dd8fda5d2315a21f7f9ad6e
elio najem <elio.najem@elbarid.com>
2023-08-17 09:20:09 +0300

    add setRoles in User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -80,7 +80,12 @@ class User implements UserInterface
         return array_unique($roles);
     }
 
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
+        return $this;
+    }
 
     /**
      * @see UserInterface
