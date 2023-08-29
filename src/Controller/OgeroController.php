c261ce6281bf965382213f69b18b21073d383cb5
Anthony Saliba <anthony.saliban@outlook.com>
2023-08-29 13:41:26 +0300

    pushed to live

--- src/Controller/OgeroController.php
+++ src/Controller/OgeroController.php
@@ -54,15 +54,15 @@ class OgeroController extends AbstractController
             $parameters['deviceType'] = $suyoolUserInfo[1];
 
 
-            // if ($notificationServices->checkUser($suyoolUserInfo[0], $suyoolUserInfo[2]) && $devicetype) {
-                // $suyoolUserId = $this->session->set('suyoolUserId', $suyoolUserInfo[0]);
-                $suyoolUserId = $this->session->set('suyoolUserId', 52);
+            if ($notificationServices->checkUser($suyoolUserInfo[0], $suyoolUserInfo[2]) && $devicetype) {
+                $suyoolUserId = $this->session->set('suyoolUserId', $suyoolUserInfo[0]);
+                // $suyoolUserId = $this->session->set('suyoolUserId', 52);
 
                 // $parameters['Test'] = "tst";
                 return $this->render('ogero/index.html.twig', ['parameters' => $parameters]);
-            // } else {
-                // return $this->render('ExceptionHandling.html.twig');
-            // }
+            } else {
+                return $this->render('ExceptionHandling.html.twig');
+            }
         } else {
             return $this->render('ExceptionHandling.html.twig');
         }
--- src/Controller/TouchController.php
+++ src/Controller/TouchController.php
@@ -57,19 +57,19 @@ class TouchController extends AbstractController
             $suyoolUserInfo = explode("!#!", $decrypted_string);
             $devicetype = stripos($useragent, $suyoolUserInfo[1]);
 
-            // if ($notificationServices->checkUser($suyoolUserInfo[0], $suyoolUserInfo[2]) && $devicetype) {
+            if ($notificationServices->checkUser($suyoolUserInfo[0], $suyoolUserInfo[2]) && $devicetype) {
                 $SuyoolUserId = $suyoolUserInfo[0];
-                // $SuyoolUserId = $this->session->set('suyoolUserId', $SuyoolUserId);
-                $SuyoolUserId = $this->session->set('suyoolUserId', 52);
+                $SuyoolUserId = $this->session->set('suyoolUserId', $SuyoolUserId);
+                // $SuyoolUserId = $this->session->set('suyoolUserId', 52);
 
                 $parameters['deviceType'] =$suyoolUserInfo[1] ;
 
                 return $this->render('touch/index.html.twig', [
                     'parameters' => $parameters
                 ]);
-            // } else {
-                // return $this->render('ExceptionHandling.html.twig');
-            // }
+            } else {
+                return $this->render('ExceptionHandling.html.twig');
+            }
         } else {
             return $this->render('ExceptionHandling.html.twig');
         }
