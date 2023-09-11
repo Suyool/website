f18ae159cb17284489656a8104a0fcf1ab92379e
Anthony Saliba <anthony.saliban@outlook.com>
2023-09-11 15:07:44 +0300

    fix bouckect option

--- assets/react/Loto/components/BouquetOptions.js
+++ assets/react/Loto/components/BouquetOptions.js
@@ -60,139 +60,140 @@ const BouquetOptions = ({
   };
   return (
     <div className="PickYourBoucket">
-      <div className="topSectionPick">
-        <div className="brBoucket"></div>
-        <div className="titles">
-          <div className="titleGrid">Bouquet Options</div>
-          <button
-            onClick={() => {
-              setShowBouquet(false);
-              setIsHide(false);
-            }}
-          >
-            Cancel
-          </button>
+      <div className="thePickGridCont">
+        <div className="topSectionPick">
+          <div className="brBoucket"></div>
+          <div className="titles">
+            <div className="titleGrid">Bouquet Options</div>
+            <button
+              onClick={() => {
+                setShowBouquet(false);
+                setIsHide(false);
+              }}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="bodySectionPick">
-        <div className="bouquetList">
-          <div className="bouquetItem">
-            <div className="checkbox">
-              <input
-                type="radio"
-                name="radio"
-                onChange={() =>
-                  handleOptionSelect({
-                    gridNb: 25,
-                    price: 25 * getBouquetgridprice,
-                  })
-                }
-              />
-            </div>
-            <div className="data">
-              <div className="basic">25 basic grids</div>
-              <div className="price">
-                {parseInt(25 * getBouquetgridprice).toLocaleString()} LBP
-              </div>
-            </div>
-          </div>
-
-          <div className="bouquetItem">
-            <div className="checkbox">
-              <input
-                type="radio"
-                name="radio"
-                onChange={() =>
-                  handleOptionSelect({
-                    gridNb: 50,
-                    price: 50 * getBouquetgridprice,
-                  })
-                }
-              />
-            </div>
-            <div className="data">
-              <div className="basic">50 basic grids</div>
-              <div className="price">
-                {parseInt(50 * getBouquetgridprice).toLocaleString()} LBP
-              </div>
-            </div>
-          </div>
-          <div className="bouquetItem">
-            <div className="checkbox">
-              <input
-                type="radio"
-                name="radio"
-                onChange={() =>
-                  handleOptionSelect({
-                    gridNb: 100,
-                    price: 100 * getBouquetgridprice,
-                  })
-                }
-              />
-            </div>
-            <div className="data">
-              <div className="basic">100 basics grids</div>
-              <div className="price">
-                {parseInt(100 * getBouquetgridprice).toLocaleString()} LBP
-              </div>
-            </div>
-          </div>
-          <div className="bouquetItem">
-            <div className="checkbox">
-              <input
-                type="radio"
-                name="radio"
-                onChange={() =>
-                  handleOptionSelect({
-                    gridNb: 500,
-                    price: 500 * getBouquetgridprice,
-                  })
-                }
-              />
-            </div>
-            <div className="data">
-              <div className="basic">500 basics grids</div>
-              <div className="price">
-                {parseInt(500 * getBouquetgridprice).toLocaleString()} LBP
-              </div>
-            </div>
-          </div>
-
-          <div className="bouquetItem">
-            <div className="checkbox">
-              <input
-                type="radio"
-                name="radio"
-                onChange={() =>
-                  handleOptionSelect({
-                    gridNb: 0,
-                    price: 0 * getBouquetgridprice,
-                  })
-                }
-              />
-            </div>
-            <div className="data">
-              <div className="basic">
+        <div className="bodySectionPick">
+          <div className="bouquetList">
+            <div className="bouquetItem">
+              <div className="checkbox">
                 <input
-                  type="number"
-                  name="selectedOption"
-                  // value=""
-                  onChange={(event) =>
+                  type="radio"
+                  name="radio"
+                  onChange={() =>
                     handleOptionSelect({
-                      gridNb: event.target.value,
-                      price: event.target.value * getBouquetgridprice,
+                      gridNb: 25,
+                      price: 25 * getBouquetgridprice,
                     })
                   }
                 />
-                Other
               </div>
-              {/* <div className="price">
+              <div className="data">
+                <div className="basic">25 basic grids</div>
+                <div className="price">
+                  {parseInt(25 * getBouquetgridprice).toLocaleString()} LBP
+                </div>
+              </div>
+            </div>
+
+            <div className="bouquetItem">
+              <div className="checkbox">
+                <input
+                  type="radio"
+                  name="radio"
+                  onChange={() =>
+                    handleOptionSelect({
+                      gridNb: 50,
+                      price: 50 * getBouquetgridprice,
+                    })
+                  }
+                />
+              </div>
+              <div className="data">
+                <div className="basic">50 basic grids</div>
+                <div className="price">
+                  {parseInt(50 * getBouquetgridprice).toLocaleString()} LBP
+                </div>
+              </div>
+            </div>
+            <div className="bouquetItem">
+              <div className="checkbox">
+                <input
+                  type="radio"
+                  name="radio"
+                  onChange={() =>
+                    handleOptionSelect({
+                      gridNb: 100,
+                      price: 100 * getBouquetgridprice,
+                    })
+                  }
+                />
+              </div>
+              <div className="data">
+                <div className="basic">100 basics grids</div>
+                <div className="price">
+                  {parseInt(100 * getBouquetgridprice).toLocaleString()} LBP
+                </div>
+              </div>
+            </div>
+            <div className="bouquetItem">
+              <div className="checkbox">
+                <input
+                  type="radio"
+                  name="radio"
+                  onChange={() =>
+                    handleOptionSelect({
+                      gridNb: 500,
+                      price: 500 * getBouquetgridprice,
+                    })
+                  }
+                />
+              </div>
+              <div className="data">
+                <div className="basic">500 basics grids</div>
+                <div className="price">
+                  {parseInt(500 * getBouquetgridprice).toLocaleString()} LBP
+                </div>
+              </div>
+            </div>
+
+            <div className="bouquetItem">
+              <div className="checkbox">
+                <input
+                  type="radio"
+                  name="radio"
+                  onChange={() =>
+                    handleOptionSelect({
+                      gridNb: 0,
+                      price: 0 * getBouquetgridprice,
+                    })
+                  }
+                />
+              </div>
+              <div className="data">
+                <div className="basic">
+                  <input
+                    type="number"
+                    name="selectedOption"
+                    // value=""
+                    onChange={(event) =>
+                      handleOptionSelect({
+                        gridNb: event.target.value,
+                        price: event.target.value * getBouquetgridprice,
+                      })
+                    }
+                  />
+                  Other
+                </div>
+                {/* <div className="price">
                 {parseInt(2 * getBouquetgridprice).toLocaleString()} LBP
               </div> */}
+              </div>
             </div>
-          </div>
 
-          {/* <div className="bouquetItem">
+            {/* <div className="bouquetItem">
             <div className="checkbox">
               <input type="radio" name="radio" />
             </div>
@@ -201,16 +202,17 @@ const BouquetOptions = ({
               <div className="price"></div>
             </div>
           </div> */}
+          </div>
         </div>
-      </div>
-      <div className="footSectionPick">
-        <button
-          className="ContinueBtn"
-          disabled={isContinueDisabled}
-          onClick={handleContinue}
-        >
-          Continue
-        </button>
+        <div className="footSectionPick">
+          <button
+            className="ContinueBtn"
+            disabled={isContinueDisabled}
+            onClick={handleContinue}
+          >
+            Continue
+          </button>
+        </div>
       </div>
     </div>
   );
