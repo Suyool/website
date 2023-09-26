c8bed5826da3ba45a0cb7fce982dffe66d6d774b
Jean-Pierre <rhjeanpierre7@gmail.com>
2023-09-26 10:54:42 +0300

    top section

--- src/Controller/HelpCenterController.php
+++ src/Controller/HelpCenterController.php
@@ -6,22 +6,28 @@ use App\Repository\QuestionRepository;
 use App\Repository\QuestionsCategoryRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelpCenterController extends AbstractController
 {
-    public function __construct(ManagerRegistry $managerRegistry){
-
+    private $request;
+    public function __construct(ManagerRegistry $managerRegistry, RequestStack $request){
+        $this->request=$request->getCurrentRequest();
     }
 
     /**
      * @Route("/help-center", name="help_center")
      */
     public function index(QuestionsCategoryRepository $categoryRepository){
+        $type = $this->request->query->get('type-id', 1);
+
+        //add the type later
         $categories = $categoryRepository->findQuestionsByCategories();
-//        dd($categories);
+        //dd($categories);
         return $this->render('helpCenter/index.html.twig', [
             'categories' => $categories,
+            'type' => $type
         ]);
     }
 
