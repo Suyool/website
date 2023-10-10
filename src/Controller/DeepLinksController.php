9ce23547fac39a24e08423fa50d80c9a483ff24d
elio najem <elio.najem@elbarid.com>
2023-10-10 14:41:14 +0300

    add encoding to url in rediction

--- src/Controller/DeepLinksController.php
+++ src/Controller/DeepLinksController.php
@@ -26,8 +26,12 @@ class DeepLinksController extends AbstractController
 
 
         if (stristr($_SERVER['HTTP_USER_AGENT'], 'mobi') !== FALSE) {
-            $redirectUrl = 'suyoolpay://suyool.com/suyool=?{"flag":"' . $flag . '","browsertype":"' . $browser . '","AdditionalInfo":"' . $additionalInfo . '","currentUrl":"' . $currentUrl . '"}';
-            $parameters['redirectUrl'] =$redirectUrl;
+            $redirectUrl = 'suyoolpay://suyool.com/suyool=' . urlencode(json_encode([
+                    'flag' => $flag,
+                    'browsertype' => $browser,
+                    'AdditionalInfo' => $additionalInfo,
+                    'currentUrl' => $currentUrl
+                ]));            $parameters['redirectUrl'] =$redirectUrl;
 
             return $this->render('deeplink/index.html.twig',$parameters);
             return new RedirectResponse($redirectUrl);
