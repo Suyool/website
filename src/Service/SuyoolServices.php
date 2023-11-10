d6de4acae8a45bdbdce1502515599ee325e97498
Anthony Saliba <anthony.saliban@outlook.com>
2023-11-10 14:26:34 +0200

    fix iveri to topup

--- src/Service/SuyoolServices.php
+++ src/Service/SuyoolServices.php
@@ -46,6 +46,11 @@ class SuyoolServices
 
     }
 
+    public static function decrypt($stringToDecrypt){
+        $decrypted_string = openssl_decrypt($stringToDecrypt, $_ENV['CIPHER_ALGORITHME'], $_ENV['DECRYPT_KEY'], 0, $_ENV['INITIALLIZATION_VECTOR']);
+        return $decrypted_string;
+    }
+
     /**
      * Push Utility Api
      */
