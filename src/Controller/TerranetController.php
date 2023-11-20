b4c6a61c11444f2cda6bb6786eed85f0d3d668fb
elio najem <elio.najem@elbarid.com>
2023-11-20 12:26:03 +0200

    change decrypt service

--- src/Controller/TerranetController.php
+++ src/Controller/TerranetController.php
@@ -41,10 +41,10 @@ class TerranetController extends AbstractController
     public function index(NotificationServices $notificationServices)
     {
         $useragent = $_SERVER['HTTP_USER_AGENT'];
-        //$_POST['infoString'] = "3mzsXlDm5DFUnNVXA5Pu8T1d5nNACEsiiUEAo7TteE/x3BGT3Oy3yCcjUHjAVYk3";
+        $_POST['infoString'] = "3mzsXlDm5DFUnNVXA5Pu8T1d5nNACEsiiUEAo7TteE/x3BGT3Oy3yCcjUHjAVYk3";
 
         if (isset($_POST['infoString'])) {
-            $decrypted_string = DecryptService::decrypt($_POST['infoString']);
+            $decrypted_string = $this->suyoolServices->decrypt($_POST['infoString']);
             $suyoolUserInfo = explode("!#!", $decrypted_string);
             $devicetype = stripos($useragent, $suyoolUserInfo[1]);
 
