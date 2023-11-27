8d1940ebf1a8b8329de38db91fe7ae9f5c2d7a6d
elio najem <elio.najem@elbarid.com>
2023-11-27 10:19:25 +0200

    add terraNetImages

--- assets/react/TerraNet/ProductList.js
+++ assets/react/TerraNet/ProductList.js
@@ -1,41 +1,52 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
-const ProductList = ({ products, setActiveButton, setSelectedProduct,setHeaderTitle,setBackLink }) => {
-
-    const onProductSelect = (product) => {
-        setActiveButton({name: "SelectedProductInfo"});
-        setSelectedProduct(product);
-    }
+const ProductList = ({
+                         products,
+                         setActiveButton,
+                         setSelectedProduct,
+                         setHeaderTitle,
+                         setBackLink,
+                     }) => {
     useEffect(() => {
         setHeaderTitle("Re-charge TerraNet");
         setBackLink("");
     }, []);
+
+    const onProductSelect = (product) => {
+        setActiveButton({ name: "SelectedProductInfo" });
+        setSelectedProduct(product);
+    };
+
     return (
         <div className="bundlesSection">
             <div className="mainTitle">Available Re-charge Packages</div>
             <div className="mainDesc">* Excluding Taxes</div>
             <div className="bundlesSection mb-5">
-                {products.map((product, index) => (
-                    <div
-                        className="bundleGrid"
-                        key={index}
-                        onClick={() => {
-                            onProductSelect(product);
-                        }}
-                    >
-                        <img
-                            className="GridImg"
-                            src="/build/images/terraNet/terraNetLogo.png"
-                            alt="bundleImg"
-                        />
-                        <div className="gridDesc">
-                            <div className="Price">
-                               L.L {parseInt(product.OriginalHT).toLocaleString()}
+                {products.map((product, index) => {
+                    const imagePath = `/build/images/terraNet/circle_product_${product.ProductId}.svg`;
+
+                    return (
+                        <div
+                            className="bundleGrid"
+                            key={index}
+                            onClick={() => {
+                                onProductSelect(product);
+                            }}
+                        >
+                            <img
+                                className="GridImg"
+                                src={imagePath}
+                                alt={product.Description}
+                            />
+                            <div className="gridDesc">
+                                <div className="Price">
+                                    L.L {parseInt(product.OriginalHT).toLocaleString()}
+                                </div>
+                                <div className="bundleName">{product.Description}</div>
                             </div>
-                            <div className="bundleName">{product.Description}</div>
                         </div>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
         </div>
     );
