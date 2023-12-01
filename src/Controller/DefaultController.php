adc1889157ee0b2105657c6319e5c33b01747476
Anthony Saliba <anthony.saliban@outlook.com>
2023-12-01 18:52:50 +0200

    2xpoints to sinneys

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1056,4 +1056,12 @@ class DefaultController extends AbstractController
         return $this->render('spinneys/index.html.twig',$parameters);
     }
 
+    /**
+     * @Route("/2xPoints", name="app_doubleyourpoints")
+     */
+    public function doublePoints(){
+        return $this->redirectToRoute("app_spinneys");
+    }
+    
+
 }
