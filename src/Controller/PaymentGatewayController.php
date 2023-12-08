24def827cdce05db69c5f453e5b22d65895c10be
elio najem <elio.najem@elbarid.com>
2023-12-08 11:55:53 +0200

    new api in paymentGateway contoller

--- src/Controller/PaymentGatewayController.php
+++ src/Controller/PaymentGatewayController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -77,5 +78,58 @@ class PaymentGatewayController extends AbstractController
     public function payWithQR()
     {
         return $this->render('paymentGateway/qr.html.twig');
+    }
+
+    /**
+     * @Route("/api/invoice", name="payment_api_invoice")
+     */
+    public function api(Request $request,SessionInterface $session)
+    {
+        $apiKeydata = "!23";
+        $data = json_decode($request->getContent(),true);
+        $userAgent = $request->headers->get('User-Agent');
+
+        $isMobile = false;
+
+        // Perform some basic checks to determine if it's a mobile user agent
+        if (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Android') !== false) {
+            $isMobile = true;
+        }
+
+        $merchantId = $data['MerchantAccountID'];
+        $order_id = $data['TransactionID'];
+        $amount = $data['Amount'];
+        $currency = $data['Currency'];
+        $order_desc = $data['AdditionalInfo'];
+        $secureHash = $data['SecureHash'];
+        $callbackURL = $data['callBackURL'];
+        $TS = $data['TS'];
+        $TranTS = $data['TranTS'];
+        $apiKey = $data['apiKey'];
+
+        if($apiKeydata == $apiKey){
+
+            // Store data in the session
+            $session->set('payment_data', [
+                'MerchantID' => $merchantId,
+                'Amount' => $amount,
+                'Currency' => $currency,
+                'SecureHash' => $secureHash,
+                'TranID' => $order_id,
+                'AdditionalInfo' => $order_desc,
+                'CallBackURL'=> $callbackURL,
+                'TS' => $TS,
+                'TranTS' => $TranTS
+            ]);
+
+            return $this->render('paymentGateway/index.html.twig',[
+                'is_mobile' => $isMobile,
+            ]);
+
+        }else {
+            return new Response("your are not eligible to continue");
+
+        }
+
     }
 }
