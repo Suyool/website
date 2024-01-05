6de82d57d97ffd44d866d4d0101a32086d5c624a
Anthony Saliba <anthony.saliban@outlook.com>
2024-01-05 09:43:15 +0200

    some js on valiidity change

--- src/Service/BobPaymentServices.php
+++ src/Service/BobPaymentServices.php
@@ -1092,7 +1092,7 @@ class BobPaymentServices
                 'imgsrc' => $imgsrc,
                 'description' => $description,
                 'button' => $button,
-                'redirect' => "test/" . $session->getOrders()->getCode()
+                'redirect' => $session->getOrders()->getCode()
             ];
             return $parameters;
         }
