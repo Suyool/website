6f9f23401c94d695f39e55855a609246fd4b6dc8
elio najem <elio.najem@elbarid.com>
2024-01-15 14:41:07 +0200

    only access api from api.suyool.com

--- src/EventListener/DomainAccessListener.php
+++ src/EventListener/DomainAccessListener.php
@@ -13,26 +13,48 @@ class DomainAccessListener
     {
         $request = $event->getRequest();
         $host = $request->getHost();
-        $allowedDomain = $this->getAllowedDomain();
 
-        if ($this->isRestrictedPath($request) && $host !== $allowedDomain) {
+        // Define the allowed domains based on the environment
+        $allowedDomain = $this->getAllowedDomain($request);
+
+        // Check if the request is for the admin panel or API and if the domain is allowed
+        if (($this->isAdminPath($request) || $this->isApiPath($request)) && $host !== $allowedDomain) {
             $response = new RedirectResponse('/');
             $event->setResponse($response);
         }
     }
 
-    private function isRestrictedPath($request)
+    private function isApiPath($request)
     {
+        // Check if the request path is within the API paths
+        return strpos($request->getPathInfo(), '/merchant/v1/invoices') === 0;
+    }
+
+    private function isAdminPath($request)
+    {
+        // Check if the request path is for the admin panel
         return strpos($request->getPathInfo(), '/admin_login') === 0;
     }
 
-    private function getAllowedDomain()
+    private function getAllowedDomain($request)
     {
         // Check the environment and get the appropriate ALLOWED_DOMAIN value
         if ($_ENV['APP_ENV'] === 'prod') {
-            return $_ENV['ALLOWED_DOMAIN_PROD'];
+            if ($this->isApiPath($request)) {
+                // Define the allowed domain for the API in production
+                return $_ENV['ALLOWED_DOMAIN_API_PROD'];
+            } else {
+                // Define the allowed domain for the admin panel in production
+                return $_ENV['ALLOWED_DOMAIN_PROD'];
+            }
         } else {
-            return $_ENV['ALLOWED_DOMAIN_DEV'] ;
+            if ($this->isApiPath($request)) {
+                // Define the allowed domain for the API in development
+                return $_ENV['ALLOWED_DOMAIN_API_DEV'];
+            } else {
+                // Define the allowed domain for the admin panel in development
+                return $_ENV['ALLOWED_DOMAIN_DEV'];
+            }
         }
     }
 }
