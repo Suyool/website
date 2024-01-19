fb2d43aa7246d31942682ff5cbe8fa031be099b8
Anthony Saliba <anthony.saliban@outlook.com>
2024-01-19 15:17:50 +0200

    terranet corporate

--- assets/react/TerraNet/App.js
+++ assets/react/TerraNet/App.js
@@ -9,142 +9,166 @@ import SelectedProductInfo from "./SelectedProductInfo";
 import LandlineForm from "./LandlineForm";
 
 const App = ({ parameters }) => {
-    const [activeButton, setActiveButton] = useState({ name: "" });
-    const [getBackLink, setBackLink] = useState({ name: "" });
-    const [getHeaderTitle, setHeaderTitle] = useState("TerraNet");
+  const [activeButton, setActiveButton] = useState({ name: "" });
+  const [getBackLink, setBackLink] = useState({ name: "" });
+  const [getHeaderTitle, setHeaderTitle] = useState("TerraNet");
 
-    const [getDataGetting, setDataGetting] = useState("");
+  const [getDataGetting, setDataGetting] = useState("");
 
-    //Modal Variable
-    const [getModalName, setModalName] = useState("");
-    const [modalShow, setModalShow] = useState(false);
-    const [getSuccessModal, setSuccessModal] = useState({
-        imgPath: "/build/images/alfa/SuccessImg.png",
-        title: "Success Modal",
-        desc: "Success Modal",
-    });
-    const [getErrorModal, setErrorModal] = useState({
-        img: "/build/images/alfa/error.png",
-        title: "Error Modal",
-        btn: "Top Up",
-        desc: "Error Modal",
-    });
+  //Modal Variable
+  const [getModalName, setModalName] = useState("");
+  const [modalShow, setModalShow] = useState(false);
+  const [apiUrl,setApiUrl] = useState(null);
+  const [getSuccessModal, setSuccessModal] = useState({
+    imgPath: "/build/images/alfa/SuccessImg.png",
+    title: "Success Modal",
+    desc: "Success Modal",
+  });
+  const [getErrorModal, setErrorModal] = useState({
+    img: "/build/images/alfa/error.png",
+    title: "Error Modal",
+    btn: "Top Up",
+    desc: "Error Modal",
+  });
 
-    //products
-    const [products, setProducts] = useState([]);
-    const [selectedProduct, setSelectedProduct] = useState(null);
+  //products
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
-    useEffect(() => {
-        setDataGetting("");
-        window.handleCheckout = (message) => {
-            setDataGetting(message);
-        };
-    });
+  useEffect(()=>{
+    if(window.REACT_APP_API_URL == "prod"){
+      setApiUrl("");
+   }else{
+      setApiUrl("http://localhost:3000/bills");
+   }
+  },[])
 
-    return (
-        <div id="TerraNetBody">
-            <Header
-                parameters={parameters}
-                activeButton={activeButton}
-                setActiveButton={setActiveButton}
-                getHeaderTitle={getHeaderTitle}
-                getBackLink={getBackLink}
+  useEffect(() => {
+    setDataGetting("");
+    const searchParams = new URLSearchParams(window.location.search);
+    const idParam = searchParams.get("comp");
+    if (idParam) {
+      setActiveButton({ name: idParam });
+      // searchParams.set("")
+    }
+    window.handleCheckout = (message) => {
+      setDataGetting(message);
+    };
+  }, []);
+
+  const handleReceiveMessage = (event) => {
+    if (typeof event.data === "string") {
+      setDataGetting(event.data);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("message", handleReceiveMessage);
+    return () => {
+      window.removeEventListener("message", handleReceiveMessage);
+    };
+  }, []);
+
+  return (
+    <div id="TerraNetBody">
+      <Header
+        parameters={parameters}
+        activeButton={activeButton}
+        setActiveButton={setActiveButton}
+        getHeaderTitle={getHeaderTitle}
+        getBackLink={getBackLink}
+        apiUrl={apiUrl}
+      />
+      <div className="scrolableView">
+        <>
+          {activeButton.name === "" && (
+            <Default
+              activeButton={activeButton}
+              setActiveButton={setActiveButton}
+              setHeaderTitle={setHeaderTitle}
+              setBackLink={setBackLink}
             />
-            <div className="scrolableView">
+          )}
 
-                <>
-                    {activeButton.name === "" && (
-                        <Default
-                            activeButton={activeButton}
-                            setActiveButton={setActiveButton}
-                            setHeaderTitle={setHeaderTitle}
-                            setBackLink={setBackLink}
-                        />
-                    )}
+          {/* TerraNet Usename */}
+          {(activeButton.name === "UsernameForm" || activeButton.name === "username") && (
+            <UsernameForm
+              setProducts={setProducts}
+              setActiveButton={setActiveButton}
+              setBackLink={setBackLink}
+              setModalShow={setModalShow}
+              setErrorModal={setErrorModal}
+              setModalName={setModalName}
+              setHeaderTitle={setHeaderTitle}
+            />
+          )}
+          {(activeButton.name === "LandlineForm" || activeButton.name === "landline") && (
+            <LandlineForm
+              setProducts={setProducts}
+              setActiveButton={setActiveButton}
+              setBackLink={setBackLink}
+              setModalShow={setModalShow}
+              setErrorModal={setErrorModal}
+              setModalName={setModalName}
+              setHeaderTitle={setHeaderTitle}
+            />
+          )}
 
-                    {/* TerraNet Usename */}
-                    {activeButton.name === "UsernameForm" && (
-                        <UsernameForm
-                            setProducts={setProducts}
-                            setActiveButton={setActiveButton}
-                            setBackLink={setBackLink}
-                            setModalShow={setModalShow}
-                            setErrorModal={setErrorModal}
-                            setModalName={setModalName}
-                            setHeaderTitle={setHeaderTitle}
-
-                        />
-                    )}
-                    {activeButton.name === "LandlineForm" && (
-                        <LandlineForm
-                            setProducts={setProducts}
-                            setActiveButton={setActiveButton}
-                            setBackLink={setBackLink}
-                            setModalShow={setModalShow}
-                            setErrorModal={setErrorModal}
-                            setModalName={setModalName}
-                            setHeaderTitle={setHeaderTitle}
-
-                        />
-                    )}
-
-                    {activeButton.name === "inputValue" && (
-                        <div id="ReCharge">
-                            <ProductList
-                                products={products}
-                                setSelectedProduct={setSelectedProduct}
-                                setActiveButton={setActiveButton}
-                                setBackLink={setBackLink}
-                                setHeaderTitle={setHeaderTitle}
-
-                            />
-                        </div>
-                    )}
-
-                    {activeButton.name === "SelectedProductInfo" && (
-                        <SelectedProductInfo
-                            selectedProduct={selectedProduct}
-                            setActiveButton={setActiveButton}
-                            setModalShow={setModalShow}
-                            setErrorModal={setErrorModal}
-                            setSuccessModal={setSuccessModal}
-                            setModalName={setModalName}
-                            activeButton={activeButton}
-                            parameters={parameters}
-                            setBackLink={setBackLink}
-                            setHeaderTitle={setHeaderTitle}
-                        />
-                    )}
-                </>
+          {activeButton.name === "inputValue" && (
+            <div id="ReCharge">
+              <ProductList
+                products={products}
+                setSelectedProduct={setSelectedProduct}
+                setActiveButton={setActiveButton}
+                setBackLink={setBackLink}
+                setHeaderTitle={setHeaderTitle}
+              />
             </div>
+          )}
 
-            {/* Modal */}
-            {getModalName === "SuccessModal" && (
-                <SuccessModal
-                    getSuccessModal={getSuccessModal}
-                    show={modalShow}
-                    onHide={() => {
-                        setModalShow(false);
-                        setModalName("");
-                        setActiveButton({ name: "" });
-                    }}
-                />
-            )}
-            {getModalName === "ErrorModal" && (
-                <ErrorModal
-                    parameters={parameters}
-                    getErrorModal={getErrorModal}
-                    show={modalShow}
-                    onHide={() => {
-                        setModalShow(false);
-                        setModalName("");
-                        setActiveButton({ name: "" });
-                    }}
-                />
-            )}
-        </div>
-    );
+          {activeButton.name === "SelectedProductInfo" && (
+            <SelectedProductInfo
+              selectedProduct={selectedProduct}
+              setActiveButton={setActiveButton}
+              setModalShow={setModalShow}
+              setErrorModal={setErrorModal}
+              setSuccessModal={setSuccessModal}
+              setModalName={setModalName}
+              activeButton={activeButton}
+              parameters={parameters}
+              setBackLink={setBackLink}
+              setHeaderTitle={setHeaderTitle}
+              apiUrl={apiUrl}
+            />
+          )}
+        </>
+      </div>
+
+      {/* Modal */}
+      {getModalName === "SuccessModal" && (
+        <SuccessModal
+          getSuccessModal={getSuccessModal}
+          show={modalShow}
+          onHide={() => {
+            setModalShow(false);
+            setModalName("");
+            setActiveButton({ name: "" });
+          }}
+        />
+      )}
+      {getModalName === "ErrorModal" && (
+        <ErrorModal
+          parameters={parameters}
+          getErrorModal={getErrorModal}
+          show={modalShow}
+          onHide={() => {
+            setModalShow(false);
+            setModalName("");
+            setActiveButton({ name: "" });
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default App;
-
