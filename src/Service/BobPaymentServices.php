eeb8f01b324a9c39279fcd225f17060c9dcd22db
Anthony Saliba <anthony.saliban@outlook.com>
2024-01-31 10:57:24 +0200

    fix amount in rtp and redirection

--- src/Service/BobPaymentServices.php
+++ src/Service/BobPaymentServices.php
@@ -1493,7 +1493,7 @@ class BobPaymentServices
                     $button = "Continue";
 
                     if ($topup[2] == 1) {
-                        $params = json_encode(['currency' => $currency, 'amount' => $amount, 'nonsuyooler' => $senderPhone]);
+                        $params = json_encode(['currency' => $currency, 'amount' => $topup[1], 'nonsuyooler' => $senderPhone]);
                         $content = $this->notificationServices->getContent('CardTopUpRtp');
                         $this->notificationServices->addNotification($suyooler, $content, $params, 0, "");
                     } else {
