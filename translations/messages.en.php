b022de47d74e16b459c96e5fbd91303bad7820ab
Anthony Saliba <anthony.saliban@outlook.com>
2024-02-07 10:16:31 +0200

    gespan landing p[ages

--- translations/messages.en.php
+++ translations/messages.en.php
@@ -438,6 +438,20 @@ return [
     "USERS_CAN_ACCESS_THEIR_MONEY_FROM_MORE_THAN_700_DrogueriePhenicia_SAD" => "In Lebanon, at over 700 BOB Finance offices. Abroad, from any ATM compatible with Visa.",
     "YOUR_SUYOOL_MASTERCARD_WILL_BE_FREE_OF_CHARGE_ADDICTS_SAD" => "Your Suyool Visa Platinum debit card will be free-of-charge as long as you are an S.A.D employee.",
 
+    //GESPASAL
+    "WHAT_ARE_THE_BENEFITS_FOR_USJ_EMPLOYEES_GESPASAL" => "What are the benefits for GESPA SAL employees?",
+    "YOU_WILL_BENEFIT_FROM_A_FREE_PLATINUM_MASTERCARD_GESPASAL" => "They will benefit from a free Visa Platinum debit card the first year & free cash withdrawal of their monthly salary as long as they are a GESPA SAL employee.",
+    "WHERE_CAN_I_WITHDRAW_MY_SALARY_IN_CASH_DrogueriePhenicia_GESPASAL" => "Where can I withdraw my payroll in cash?",
+    "USERS_CAN_ACCESS_THEIR_MONEY_FROM_MORE_THAN_700_DrogueriePhenicia_GESPASAL" => "In Lebanon, at over 700 BOB Finance offices. Abroad, from any ATM compatible with Visa.",
+    "YOUR_SUYOOL_MASTERCARD_WILL_BE_FREE_OF_CHARGE_ADDICTS_GESPASAL" => "Your Suyool Visa Platinum debit card will be free-of-charge as long as you are a GESPA SAL employee.",
+
+    //GESPASALINT
+    "WHAT_ARE_THE_BENEFITS_FOR_USJ_EMPLOYEES_GESPASALINT" => "What are the benefits for GESPA International SAL employees?",
+    "YOU_WILL_BENEFIT_FROM_A_FREE_PLATINUM_MASTERCARD_GESPASALINT" => "They will benefit from a free Visa Platinum debit card the first year & free cash withdrawal of their monthly salary as long as they are a GESPA International SAL employee.",
+    "WHERE_CAN_I_WITHDRAW_MY_SALARY_IN_CASH_DrogueriePhenicia_GESPASALINT" => "Where can I withdraw my payroll in cash?",
+    "USERS_CAN_ACCESS_THEIR_MONEY_FROM_MORE_THAN_700_DrogueriePhenicia_GESPASALINT" => "In Lebanon, at over 700 BOB Finance offices. Abroad, from any ATM compatible with Visa.",
+    "YOUR_SUYOOL_MASTERCARD_WILL_BE_FREE_OF_CHARGE_ADDICTS_GESPASALINT" => "Your Suyool Visa Platinum debit card will be free-of-charge as long as you are a GESPA International SAL employee.",
+
     //fig
     "WHAT_ARE_THE_BENEFITS_FOR_USJ_EMPLOYEES_FIG" => "What are the benefits for Frem Industrial Group employees?",
     "YOU_WILL_BENEFIT_FROM_A_FREE_PLATINUM_MASTERCARD_FIG" => "They will benefit from a free Visa Platinum debit card & free cash withdrawal of their monthly salary as long as they are a Frem Industrial Group employee.",
