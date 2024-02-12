5e75c049913d9e9c8e3af3e0c289b092d22fbfd4
YAMMOUNII <elie.yammouny@hotmail.com>
2024-02-12 17:12:02 +0200

    add image and handle ui

--- assets/react/Alfa/ReCharge.js
+++ assets/react/Alfa/ReCharge.js
@@ -1,28 +1,21 @@
 import React, { useEffect, useState } from "react";
 import ContentLoader from "react-content-loader";
 
-const ReCharge = ({
-  parameters,
-  setPrepaidVoucher,
-  getVoucherData,
-  setActiveButton,
-  setHeaderTitle,
-  setBackLink,
-}) => {
-  const [ filteredData, setFilteredData ] = useState([]);
-  const [ getLoading, setLoading ] = useState(true);
+const ReCharge = ({ parameters, setPrepaidVoucher, getVoucherData, setActiveButton, setHeaderTitle, setBackLink }) => {
+  const [filteredData, setFilteredData] = useState([]);
+  const [getLoading, setLoading] = useState(true);
 
   useEffect(() => {
     setHeaderTitle("Re-charge Alfa");
     setBackLink("");
     setFilteredData(Object.values(getVoucherData));
-  }, [ getVoucherData ]);
+  }, [getVoucherData]);
 
   useEffect(() => {
     if (filteredData.length > 0) {
       setLoading(false);
     }
-  }, [ filteredData ]);
+  }, [filteredData]);
   // console.log(filteredData);
 
   return (
@@ -31,13 +24,7 @@ const ReCharge = ({
         <div className="mainTitle">Available Re-charge Packages</div>
         <div className="mainDesc">* Excluding Taxes</div>
         {getLoading ? (
-          <ContentLoader
-            speed={2}
-            width="100%"
-            height="90vh"
-            backgroundColor="#f3f3f3"
-            foregroundColor="#ecebeb"
-          >
+          <ContentLoader speed={2} width="100%" height="90vh" backgroundColor="#f3f3f3" foregroundColor="#ecebeb">
             <rect x="0" y="0" rx="3" ry="3" width="100%" height="80" />
             <rect x="0" y="90" rx="3" ry="3" width="100%" height="80" />
             <rect x="0" y="180" rx="3" ry="3" width="100%" height="80" />
@@ -47,48 +34,50 @@ const ReCharge = ({
           </ContentLoader>
         ) : (
           <>
-            {filteredData.map((record, index) => (
-              <div
-                className="bundleGrid"
-                key={index}
-                style={
-                  record.isinstock == 0
-                    ? { display: "none" }
-                    : { display: "flex" }
-                }
-                onClick={() => {
-                  setActiveButton({ name: "MyBundle" });
-                  setPrepaidVoucher({
-                    vouchercategory: record.vouchercategory,
-                    vouchertype: record.vouchertype,
-                    priceLBP: record.priceLBP,
-                    priceUSD: record.priceUSD,
-                    desc: record.desc,
-                    isavailable: record.isavailable,
-                    desc1: record.desc1,
-                    desc2: record.desc2,
-                    beforeTaxes:record.beforeTaxes,
-                    fees:record.fees,
-                    sayrafa:record.sayrafa
-                  });
-                }}
-              >
-                <img
-                  className="GridImg"
-                  src={`/build/images/alfa/bundleImg${record.vouchertype}h.png`}
-                  alt="bundleImg"
-                />
-                <div className="gridDesc">
-                  <div className="Price">
-                    ${record.beforeTaxes}{" "}
-                    {/* <span>
+            {filteredData.length == 0 ? (
+              <div className="noData">
+                <img src="/build/images/productOut.svg" />
+                <div className="title">Product out of stock</div>
+                <div className="desc">Once we re-stock you will see the products here.</div>
+              </div>
+            ) : (
+              <>
+                {filteredData.map((record, index) => (
+                  <div
+                    className="bundleGrid"
+                    key={index}
+                    style={record.isinstock == 0 ? { display: "none" } : { display: "flex" }}
+                    onClick={() => {
+                      setActiveButton({ name: "MyBundle" });
+                      setPrepaidVoucher({
+                        vouchercategory: record.vouchercategory,
+                        vouchertype: record.vouchertype,
+                        priceLBP: record.priceLBP,
+                        priceUSD: record.priceUSD,
+                        desc: record.desc,
+                        isavailable: record.isavailable,
+                        desc1: record.desc1,
+                        desc2: record.desc2,
+                        beforeTaxes: record.beforeTaxes,
+                        fees: record.fees,
+                        sayrafa: record.sayrafa,
+                      });
+                    }}
+                  >
+                    <img className="GridImg" src={`/build/images/alfa/bundleImg${record.vouchertype}h.png`} alt="bundleImg" />
+                    <div className="gridDesc">
+                      <div className="Price">
+                        ${record.beforeTaxes}{" "}
+                        {/* <span>
                       (LBP {parseInt(record.priceLBP).toLocaleString()})
                     </span> */}
+                      </div>
+                      <div className="bundleName">{record.desc3}</div>
+                    </div>
                   </div>
-                  <div className="bundleName">{record.desc3}</div>
-                </div>
-              </div>
-            ))}
+                ))}
+              </>
+            )}
           </>
         )}
       </div>
