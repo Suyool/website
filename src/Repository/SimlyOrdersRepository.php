5f27ddf31ffcf45d31a30cc2f68e7eaa1b0a8f0a
Jean-Pierre <rhjeanpierre7@gmail.com>
2024-02-16 09:53:49 +0200

    added simly entities

---
+++ src/Repository/SimlyOrdersRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Sodetel\Order;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends EntityRepository<Order>
+ *
+ * @method Order|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Order|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Order[]    findAll()
+ * @method Order[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SimlyOrdersRepository extends EntityRepository
+{
+}
