a134a5ddd7360794da5fe3174b903968904f9fec
Anthony Saliba <anthony.saliban@outlook.com>
2024-02-21 13:26:27 +0200

    all

--- assets/react/Simly/RechargeThePayment.js
+++ assets/react/Simly/RechargeThePayment.js
@@ -1,19 +1,70 @@
 import React, { useEffect, useState } from "react";
+import QrDownload from "../Components/QrDownload";
+import { async } from "regenerator-runtime";
 const RechargeThePayment = ({ setHeaderTitle, setBackLink }) => {
   const [getselectedBtn, setselectedBtn] = useState("qr");
+  const [qr, setQr] = React.useState(null);
+
   useEffect(() => {
     setHeaderTitle("Suyool eSim");
     setBackLink("");
   }, []);
 
+
+  const handleDownload = async (e) => {
+    try {
+      const result = await fetch(localStorage.getItem("qrImage"), {
+        method: "GET",
+        headers: {},
+      });
+      const blob = await result.blob();
+      const url = URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = localStorage.getItem("qrImage");
+      link.click();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const copyToClipboard = (data) => {
+    // Create a textarea element
+    const textarea = document.createElement('textarea');
+    // Set the value of the textarea to the data to be copied
+    textarea.value = data;
+    // Append the textarea to the document body
+    document.body.appendChild(textarea);
+    // Select the text inside the textarea
+    textarea.select();
+    // Copy the selected text to the clipboard using the Clipboard API
+    document.execCommand('copy');
+    // Remove the textarea from the document body
+    document.body.removeChild(textarea);
+  };
+
   return (
     <div id="RechargeThePayment_simly">
-      <div className="MainTitle">Activate your eSIM in two ways - QR code or manually. Choose your preference:</div>
+      <div className="MainTitle">
+        Activate your eSIM in two ways - QR code or manually. Choose your
+        preference:
+      </div>
       <div className="Switch">
-        <div className={getselectedBtn === "qr" ? "selectBtnSelected" : "selectBtn"} onClick={() => setselectedBtn("qr")}>
+        <div
+          className={
+            getselectedBtn === "qr" ? "selectBtnSelected" : "selectBtn"
+          }
+          onClick={() => setselectedBtn("qr")}
+        >
           QR CODE
         </div>
-        <div className={getselectedBtn === "manual" ? "selectBtnSelected" : "selectBtn"} onClick={() => setselectedBtn("manual")}>
+        <div
+          className={
+            getselectedBtn === "manual" ? "selectBtnSelected" : "selectBtn"
+          }
+          onClick={() => setselectedBtn("manual")}
+        >
           MANUAL
         </div>
       </div>
@@ -22,47 +73,75 @@ const RechargeThePayment = ({ setHeaderTitle, setBackLink }) => {
         <>
           <div className="QrBox">
             <div className="title">This is your purchased eSim!</div>
-            <img className="image" src="/build/images/gettheapp.png" alt="qrCode" />
-            <div className="downloadBtn"><img src="/build/images/install.svg" alt="download" /></div>
+            <img
+              className="image"
+              src={localStorage.getItem("qrImage")}
+              alt="qrCode"
+            />
+            <div className="downloadBtn" onClick={handleDownload}>
+              <img src="/build/images/install.svg" alt="download"  />
+            </div>
           </div>
 
           <div className="Caution">
             <div className="warImg">
               <img src="/build/images/Loto/warning.png" alt="warning" />
             </div>
-            <div className="title">Usually, eSIMs can only be installed once. Once removed, you can’t reinstall them.</div>
+            <div className="title">
+              Usually, eSIMs can only be installed once. Once removed, you can’t
+              reinstall them.
+            </div>
           </div>
 
-          <div className="titleDes">Follow these steps to install your eSIM:</div>
+          <div className="titleDes">
+            Follow these steps to install your eSIM:
+          </div>
 
           <div className="stepsToRecharge">
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Save the QR code to your photos or take a screenshot of this screen.</div>
+              <div className="textStep">
+                Save the QR code to your photos or take a screenshot of this
+                screen.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Go to settings - Cellular - Add eSIM.</div>
+              <div className="textStep">
+                Go to settings - Cellular - Add eSIM.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Tap on ‘Use QR Code’ then ‘Open Photos’ & select the screenshot.</div>
+              <div className="textStep">
+                Tap on ‘Use QR Code’ then ‘Open Photos’ & select the screenshot.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Label the eSIM as ‘Simly - People’s Republic of China’.</div>
+              <div className="textStep">
+                Label the eSIM as ‘Simly - People’s Republic of China’.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Choose your primary line to call or send messages.</div>
+              <div className="textStep">
+                Choose your primary line to call or send messages.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Choose your primary line to use iMessage & FaceTime.</div>
+              <div className="textStep">
+                Choose your primary line to use iMessage & FaceTime.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Choose the eSIM plan as your default line for Cellular Data & do not turn on ‘Allow Cellular Data Switching’ to prevent charges on your other line.</div>
+              <div className="textStep">
+                Choose the eSIM plan as your default line for Cellular Data & do
+                not turn on ‘Allow Cellular Data Switching’ to prevent charges
+                on your other line.
+              </div>
             </div>
           </div>
           <div className="stepsToRecharge mt-4">
@@ -71,21 +150,28 @@ const RechargeThePayment = ({ setHeaderTitle, setBackLink }) => {
             <div className="subTitle">Data Roaming: ON</div>
             <div className="steps mt-2">
               <div className="dot"></div>
-              <div className="textStep">Select your Simly eSIM under ‘Cellular Plans’.</div>
+              <div className="textStep">
+                Select your Simly eSIM under ‘Cellular Plans’.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Ensure that ‘Turn On This Line’ is toggled ON.</div>
+              <div className="textStep">
+                Ensure that ‘Turn On This Line’ is toggled ON.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Go to ‘Network Selection’ & select the network.</div>
+              <div className="textStep">
+                Go to ‘Network Selection’ & select the network.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
               <div className="textStep">Turn on the data roaming.</div>
             </div>
           </div>
+          {/* <QrDownload qrCodeData={qr} setQrData={setQr} /> */}
         </>
       )}
 
@@ -95,20 +181,24 @@ const RechargeThePayment = ({ setHeaderTitle, setBackLink }) => {
             <div className="title">This is your purchased eSim!</div>
 
             <div className="qrString">
-                <div className="cardNumber">
-                    <div className="text">
-                        <div className="title">SM-DP+ ADDRESS</div>
-                        <div className="desc">Consumer.rsp.global</div>
-                    </div>
-                    <div className="copy"><img src="/build/images/copy.svg" alt="copy" /></div>
+              <div className="cardNumber">
+                <div className="text">
+                  <div className="title">SM-DP+ ADDRESS</div>
+                  <div className="desc">{localStorage.getItem("qrString")}</div>
                 </div>
-                <div className="cardNumber">
-                    <div className="text">
-                        <div className="title">SM-DP+ ADDRESS</div>
-                        <div className="desc">Consumer.rsp.global</div>
-                    </div>
-                    <div className="copy"><img src="/build/images/copy.svg" alt="copy" /></div>
+                <div className="copy">
+                  <img src="/build/images/copy.svg" alt="copy" onClick={()=>copyToClipboard(localStorage.getItem("qrString"))} />
                 </div>
+              </div>
+              <div className="cardNumber">
+                <div className="text">
+                  <div className="title">ACTIVATION CODE</div>
+                  <div className="desc">{localStorage.getItem("qrString")}</div>
+                </div>
+                <div className="copy">
+                  <img src="/build/images/copy.svg" alt="copy" onClick={()=>copyToClipboard(localStorage.getItem("qrString"))} />
+                </div>
+              </div>
             </div>
           </div>
 
@@ -116,15 +206,22 @@ const RechargeThePayment = ({ setHeaderTitle, setBackLink }) => {
             <div className="warImg">
               <img src="/build/images/Loto/warning.png" alt="warning" />
             </div>
-            <div className="title">Usually, eSIMs can only be installed once. Once removed, you can’t reinstall them.</div>
+            <div className="title">
+              Usually, eSIMs can only be installed once. Once removed, you can’t
+              reinstall them.
+            </div>
           </div>
 
-          <div className="titleDes">Follow these steps to install your eSIM:</div>
+          <div className="titleDes">
+            Follow these steps to install your eSIM:
+          </div>
 
           <div className="stepsToRecharge">
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Go to Settings - Cellular/Mobile - Add Cellular/Mobile Plan.</div>
+              <div className="textStep">
+                Go to Settings - Cellular/Mobile - Add Cellular/Mobile Plan.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
@@ -132,7 +229,9 @@ const RechargeThePayment = ({ setHeaderTitle, setBackLink }) => {
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Enter your SM-DP+ Address and Activation Code.</div>
+              <div className="textStep">
+                Enter your SM-DP+ Address and Activation Code.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
@@ -140,15 +239,23 @@ const RechargeThePayment = ({ setHeaderTitle, setBackLink }) => {
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Choose your primary line to call or send messages.</div>
+              <div className="textStep">
+                Choose your primary line to call or send messages.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Choose your primary line to use iMessage & FaceTime.</div>
+              <div className="textStep">
+                Choose your primary line to use iMessage & FaceTime.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Choose the eSIM plan as your default line for Cellular Data & do not turn on ‘Allow Cellular Data Switching’ to prevent charges on your other line.</div>
+              <div className="textStep">
+                Choose the eSIM plan as your default line for Cellular Data & do
+                not turn on ‘Allow Cellular Data Switching’ to prevent charges
+                on your other line.
+              </div>
             </div>
           </div>
           <div className="stepsToRecharge mt-4">
@@ -157,15 +264,21 @@ const RechargeThePayment = ({ setHeaderTitle, setBackLink }) => {
             <div className="subTitle">Data Roaming: ON</div>
             <div className="steps mt-2">
               <div className="dot"></div>
-              <div className="textStep">Select your Simly eSIM under ‘Cellular Plans’.</div>
+              <div className="textStep">
+                Select your Simly eSIM under ‘Cellular Plans’.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Ensure that ‘Turn On This Line’ is toggled ON.</div>
+              <div className="textStep">
+                Ensure that ‘Turn On This Line’ is toggled ON.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
-              <div className="textStep">Go to ‘Network Selection’ & select the network.</div>
+              <div className="textStep">
+                Go to ‘Network Selection’ & select the network.
+              </div>
             </div>
             <div className="steps">
               <div className="dot"></div>
