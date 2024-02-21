98e626cbd1db2961db1af3d6019d376aca504e3c
YAMMOUNII <elie.yammouny@hotmail.com>
2024-02-21 17:29:58 +0200

    still working on ui

--- assets/react/Simly/Header.js
+++ assets/react/Simly/Header.js
@@ -26,22 +26,20 @@ const Header = ({
 
     };
 
-    return (
-        <div id="MobileHeader" className={` ${
-            getSpinnerLoader ? "packagesinfo hideBackk" : ""
-        }`}>
-            <div
-                className="back"
-                onClick={() => {
-                    handleButtonClick(getBackLink);
-                }}
-            >
-                <img src="/build/images/alfa/Back.png" alt="Back"/>
-            </div>
-            <div className="headerTitle">{getHeaderTitle}</div>
-            <div className="empty"></div>
-        </div>
-    );
+  return (
+    <div id="MobileHeader">
+      <div
+        className="back"
+        onClick={() => {
+          handleButtonClick(getBackLink);
+        }}
+      >
+        <img src="/build/images/arrowBack.svg" alt="Back" />
+      </div>
+      <div className="headerTitle">{getHeaderTitle}</div>
+      <div className="empty"></div>
+    </div>
+  );
 };
 
 export default Header;
