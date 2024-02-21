29aa40817e8ccc61315de669a5e09103bc90565e
Anthony Saliba <anthony.saliban@outlook.com>
2024-02-21 15:57:00 +0200

    fix ui

--- assets/react/Simly/packages.js
+++ assets/react/Simly/packages.js
@@ -3,7 +3,15 @@ import axios from "axios";
 import ContentLoader from "react-content-loader";
 import PackageItems from "./PackageItems";
 
-const Packages = ({ setSelectedPlan, setSpinnerLoader, setActiveButton, setSelectedPackage, setBackLink, isPackageItem, setIsPackageItem }) => {
+const Packages = ({
+  setSelectedPlan,
+  setSpinnerLoader,
+  setActiveButton,
+  setSelectedPackage,
+  setBackLink,
+  isPackageItem,
+  setIsPackageItem,
+}) => {
   const [view, setView] = useState("countries");
   const [selectedData, setSelectedData] = useState([]);
   const [selectedDataLocal, setSelectedDataLocal] = useState([]);
@@ -52,7 +60,10 @@ const Packages = ({ setSelectedPlan, setSpinnerLoader, setActiveButton, setSelec
         }
 
         const continent = Object.keys(continentObj)[0];
-        const countries = continentObj[continent]?.filter((country) => country.name.toLowerCase().includes(query.toLowerCase())) || [];
+        const countries =
+          continentObj[continent]?.filter((country) =>
+            country.name.toLowerCase().includes(query.toLowerCase())
+          ) || [];
 
         if (countries.length > 0) {
           return { [continent]: countries };
@@ -128,15 +139,25 @@ const Packages = ({ setSelectedPlan, setSpinnerLoader, setActiveButton, setSelec
           <div className="search-icon-left">
             <img src="/build/images/g2g/search.svg" alt="Search Icon" />
           </div>
-          <input type="text" placeholder="Search Destination" value={searchQuery} onChange={handleSearchChange} />
-          <div className="search-icon-right" onClick={() => setActiveButton({ name: "Account" })}>
+          <input
+            type="text"
+            placeholder="Search Destination"
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+          <div
+            className="search-icon-right"
+            onClick={() => setActiveButton({ name: "Account" })}
+          >
             <img src="/build/images/topUpSimIcon.svg" alt="Icon" />
           </div>
         </div>
         <div className="filter-btns-cont">
           <div className="btnCon  d-flex justify-content-center">
             <button
-              className={"btn btn-primary " + (view === "countries" ? "active" : "")}
+              className={
+                "btn btn-primary " + (view === "countries" ? "active" : "")
+              }
               onClick={() => {
                 localStorage.setItem("parentPlanType", "Local");
                 setView("countries");
@@ -148,7 +169,9 @@ const Packages = ({ setSelectedPlan, setSpinnerLoader, setActiveButton, setSelec
           </div>
           <div className="btnCon  d-flex justify-content-center">
             <button
-              className={"btn btn-primary " + (view === "regions" ? "active" : "")}
+              className={
+                "btn btn-primary " + (view === "regions" ? "active" : "")
+              }
               onClick={() => {
                 localStorage.setItem("parentPlanType", "Regional");
                 setView("regions");
@@ -160,7 +183,9 @@ const Packages = ({ setSelectedPlan, setSpinnerLoader, setActiveButton, setSelec
           </div>
           <div className="btnCon d-flex justify-content-center">
             <button
-              className={"btn btn-primary " + (view === "global" ? "active" : "")}
+              className={
+                "btn btn-primary " + (view === "global" ? "active" : "")
+              }
               onClick={() => {
                 localStorage.setItem("parentPlanType", "Global");
                 setView("global");
@@ -171,70 +196,135 @@ const Packages = ({ setSelectedPlan, setSpinnerLoader, setActiveButton, setSelec
             </button>
           </div>
         </div>
-
-        {!isPackageItem && !selectedCountry && (
-          <>
-            {view === "regions" && (
-              <div className="row">
-                <div className="col">
-                  {selectedData &&
-                    selectedData.regional &&
-                    selectedData.regional.map((region, index) => (
-                      <div key={index} className="card mb-3" onClick={() => handleClick(region.isoCode)}>
-                        <div className="card-body">
-                          <div id="Topp">
-                            <img src={region.countryImageURL} alt={region.name} width={50} />
-                            <div className="noTopp">
-                              <div className="card-title">{region.name} Packages</div>
-                              <p className="card-text">{region.destinations} destinations</p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    ))}
-                </div>
-              </div>
-            )}
-
-            {view === "global" && (
-              <div className="row">
-                <div className="col">
-                  {selectedData &&
-                    selectedData.global &&
-                    selectedData.global.map((globalItem, index) => (
-                      <div key={index} className="card mb-3" onClick={() => handleClick(globalItem.isoCode)}>
-                        <div className="card-body">
-                          <div id="Topp">
-                            <img src={globalItem.countryImageURL} alt={globalItem.name} width={50} />
-                            <div className="noTopp">
-                              <div className="card-title">{globalItem.name} Packages</div>
-                              <p className="card-text">{globalItem.destinations} destinations</p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    ))}
-                </div>
-              </div>
-            )}
-          </>
-        )}
       </div>
       {isPackageItem && selectedCountry ? (
-        <PackageItems country={selectedCountry} setSelectedPlan={setSelectedPlan} setActiveButton={setActiveButton} setSelectedPackage={setSelectedPackage} />
+        <PackageItems
+          country={selectedCountry}
+          setSelectedPlan={setSelectedPlan}
+          setActiveButton={setActiveButton}
+          setSelectedPackage={setSelectedPackage}
+        />
       ) : (
         <>
+          { (
+            <>
+              {view === "regions" && (
+                <div className="row">
+                  <div className="col">
+                    {selectedData &&
+                      selectedData.regional &&
+                      selectedData.regional.map((region, index) => (
+                        <div
+                          key={index}
+                          className="card mb-3"
+                          onClick={() => handleClick(region.isoCode)}
+                        >
+                          <div className="card-body">
+                            <div id="Topp">
+                              <img
+                                src={region.countryImageURL}
+                                alt={region.name}
+                                width={50}
+                              />
+                              <div className="noTopp">
+                                <div className="card-title">
+                                  {region.name} Packages
+                                </div>
+                                <p className="card-text">
+                                  {region.destinations} destinations
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                  </div>
+                </div>
+              )}
+
+              {view === "global" && (
+                <div className="row">
+                  <div className="col">
+                    {selectedData &&
+                      selectedData.global &&
+                      selectedData.global.map((globalItem, index) => (
+                        <div
+                          key={index}
+                          className="card mb-3"
+                          onClick={() => handleClick(globalItem.isoCode)}
+                        >
+                          <div className="card-body">
+                            <div id="Topp">
+                              <img
+                                src={globalItem.countryImageURL}
+                                alt={globalItem.name}
+                                width={50}
+                              />
+                              <div className="noTopp">
+                                <div className="card-title">
+                                  {globalItem.name} Packages
+                                </div>
+                                <p className="card-text">
+                                  {globalItem.destinations} destinations
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                  </div>
+                </div>
+              )}
+            </>
+          )}
           {view === "countries" && (
             <>
               {isLoading ? (
-                <div className="row ps-3" style={{ width: "100%", overflow: "hidden" }}>
-                  <ContentLoader speed={2} width="100%" height="90vh" backgroundColor="#f3f3f3" foregroundColor="#ecebeb">
+                <div
+                  className="row ps-3"
+                  style={{ width: "100%", overflow: "hidden" }}
+                >
+                  <ContentLoader
+                    speed={2}
+                    width="100%"
+                    height="90vh"
+                    backgroundColor="#f3f3f3"
+                    foregroundColor="#ecebeb"
+                  >
                     <rect x="0" y="0" rx="3" ry="3" width="100%" height="80" />
                     <rect x="0" y="90" rx="3" ry="3" width="100%" height="80" />
-                    <rect x="0" y="180" rx="3" ry="3" width="100%" height="80" />
-                    <rect x="0" y="270" rx="3" ry="3" width="100%" height="80" />
-                    <rect x="0" y="360" rx="3" ry="3" width="100%" height="80" />
-                    <rect x="0" y="450" rx="3" ry="3" width="100%" height="80" />
+                    <rect
+                      x="0"
+                      y="180"
+                      rx="3"
+                      ry="3"
+                      width="100%"
+                      height="80"
+                    />
+                    <rect
+                      x="0"
+                      y="270"
+                      rx="3"
+                      ry="3"
+                      width="100%"
+                      height="80"
+                    />
+                    <rect
+                      x="0"
+                      y="360"
+                      rx="3"
+                      ry="3"
+                      width="100%"
+                      height="80"
+                    />
+                    <rect
+                      x="0"
+                      y="450"
+                      rx="3"
+                      ry="3"
+                      width="100%"
+                      height="80"
+                    />
                   </ContentLoader>
                 </div>
               ) : (
@@ -246,21 +336,36 @@ const Packages = ({ setSelectedPlan, setSpinnerLoader, setActiveButton, setSelec
                           <div key={index} className="continent-container">
                             {Object.keys(continentObj)?.map((continent) => (
                               <React.Fragment key={continent}>
-                                <div className="title">{displaycontinent(continent)}</div>
+                                <div className="title">
+                                  {displaycontinent(continent)}
+                                </div>
                                 <div className="country-scroll-container">
                                   <div className="row flex-nowrap">
-                                    {continentObj[continent].map((country, idx) => (
-                                      <div className="imgText">
-                                        <div key={idx} className="">
-                                          <div className="card countryCard" onClick={() => handleClick(country.isoCode)}>
-                                            <div className="card-body">
-                                              <img src={country.countryImageURL} alt={country.name} width={50} />
+                                    {continentObj[continent].map(
+                                      (country, idx) => (
+                                        <div className="imgText">
+                                          <div key={idx} className="">
+                                            <div
+                                              className="card countryCard"
+                                              onClick={() =>
+                                                handleClick(country.isoCode)
+                                              }
+                                            >
+                                              <div className="card-body">
+                                                <img
+                                                  src={country.countryImageURL}
+                                                  alt={country.name}
+                                                  width={50}
+                                                />
+                                              </div>
                                             </div>
                                           </div>
+                                          <h5 className="card-title">
+                                            {country.name}
+                                          </h5>
                                         </div>
-                                        <h5 className="card-title">{country.name}</h5>
-                                      </div>
-                                    ))}
+                                      )
+                                    )}
                                   </div>
                                 </div>
                               </React.Fragment>
