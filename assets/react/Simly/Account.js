532fabc1a08878ff5133bfa81710d257002ce6e8
Anthony Saliba <anthony.saliban@outlook.com>
2024-02-27 09:59:38 +0200

    disable toup on terminated

--- assets/react/Simly/Account.js
+++ assets/react/Simly/Account.js
@@ -3,7 +3,24 @@ import React, { useEffect, useState } from "react";
 import ContentLoader from "react-content-loader";
 import { Spinner } from "react-bootstrap";
 
-const Account = ({ setHeaderTitle, parameters, selectedPlan, setActiveButton, selectedPackage, setEsimDetail, setBackLink, getDataGetting, setDataGetting, setErrorModal, setSuccessModal, setModalName, setModalShow, setSpinnerLoader, getSpinnerLoader, setEsimId }) => {
+const Account = ({
+  setHeaderTitle,
+  parameters,
+  selectedPlan,
+  setActiveButton,
+  selectedPackage,
+  setEsimDetail,
+  setBackLink,
+  getDataGetting,
+  setDataGetting,
+  setErrorModal,
+  setSuccessModal,
+  setModalName,
+  setModalShow,
+  setSpinnerLoader,
+  getSpinnerLoader,
+  setEsimId,
+}) => {
   const [getAccountInformation, setAccountInformation] = useState();
   const [getMap, setMap] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
@@ -50,7 +67,9 @@ const Account = ({ setHeaderTitle, parameters, selectedPlan, setActiveButton, se
         }, 2000);
       } else if (parameters?.deviceType === "Iphone") {
         setTimeout(() => {
-          window.webkit.messageHandlers.callbackHandler.postMessage("fingerprint");
+          window.webkit.messageHandlers.callbackHandler.postMessage(
+            "fingerprint"
+          );
         }, 2000);
       }
       window.handleCheckout = (message) => {
@@ -127,15 +146,29 @@ const Account = ({ setHeaderTitle, parameters, selectedPlan, setActiveButton, se
   return (
     <>
       {getSpinnerLoader && (
-        <div className={` ${getSpinnerLoader ? "accountInfo hideBackk" : "accountInfo"}`}>
+        <div
+          className={` ${
+            getSpinnerLoader ? "accountInfo hideBackk" : "accountInfo"
+          }`}
+        >
           <div id="spinnerLoader">
-            <Spinner className="spinner" animation="border" variant="secondary" />
+            <Spinner
+              className="spinner"
+              animation="border"
+              variant="secondary"
+            />
           </div>
         </div>
       )}
       {isLoading ? (
         <div className="mt-5" style={{ margin: "0 10px" }}>
-          <ContentLoader speed={2} width="100%" height="90vh" backgroundColor="#f3f3f3" foregroundColor="#ecebeb">
+          <ContentLoader
+            speed={2}
+            width="100%"
+            height="90vh"
+            backgroundColor="#f3f3f3"
+            foregroundColor="#ecebeb"
+          >
             <rect x="0" y="0" rx="3" ry="3" width="100%" height="180" />
             <rect x="0" y="210" rx="3" ry="3" width="100%" height="180" />
           </ContentLoader>
@@ -149,12 +182,23 @@ const Account = ({ setHeaderTitle, parameters, selectedPlan, setActiveButton, se
                   <div key={index} className="accountcomp">
                     <div className="accountCard">
                       <div className="titleaccount">
-                        {data.countryImage ? <img src={data.countryImage} /> : <img src="/build/images/simlyIcon.svg" />}
+                        {data.countryImage ? (
+                          <img src={data.countryImage} />
+                        ) : (
+                          <img src="/build/images/simlyIcon.svg" />
+                        )}
                         <span>{data.country}</span>
                       </div>
                       <div className="rechargable">
                         <div className="single-chart">
-                          <svg viewBox="0 0 36 36" className={`circular-chart ${data.sim.status === "FULLY_USED" ? "violet" : "green"}`}>
+                          <svg
+                            viewBox="0 0 36 36"
+                            className={`circular-chart ${
+                              data.sim.status === "FULLY_USED"
+                                ? "violet"
+                                : "green"
+                            }`}
+                          >
                             <path
                               className="circle-bg"
                               d="M18 2.0845
@@ -172,38 +216,70 @@ const Account = ({ setHeaderTitle, parameters, selectedPlan, setActiveButton, se
                               {data.sim.consumed}GB
                             </text>
                           </svg>
-                          <div className="used">used from {data.sim.size} GB</div>
+                          <div className="used">
+                            used from {data.sim.size} GB
+                          </div>
                         </div>
                         <div className="radio">
-                          <input type="checkbox" id="eSim" name="eSim" value="eSim" checked={data.sim.status !== "FULLY_USED"} disabled />
+                          <input
+                            type="checkbox"
+                            id="eSim"
+                            name="eSim"
+                            value="eSim"
+                            checked={data.sim.status !== "FULLY_USED"}
+                            disabled
+                          />
                           <label className="esim">eSim is still valid</label>
                           <br />
-                          <input type="checkbox" id="plans" name="plans" value="plan" checked={data.sim.status === "FULLY_USED"} disabled />
-                          <label className="esim">Plan has been fully used</label>
+                          <input
+                            type="checkbox"
+                            id="plans"
+                            name="plans"
+                            value="plan"
+                            checked={data.sim.status === "FULLY_USED"}
+                            disabled
+                          />
+                          <label className="esim">
+                            Plan has been fully used
+                          </label>
                         </div>
                       </div>
                       <div className="btns">
-                        <div className={data.sim.status !== "PENDING" ? "topup" : "details"}>
-                          <button
-                            className="btntopup"
-                            onClick={() => {
-                              setReqObj({
-                                esimId: data?.esimId,
-                                planId: data?.plan,
-                                countryImage: data?.countryImage,
-                              });
-                              setReqObjOpt({
-                                gb: data?.sim?.size,
-                                amount: data?.initialPrice,
-                                country: data.country,
-                              });
-                              setIsViewNetwork(true);
-                            }}
+                        {data.sim.status !== "TERMINATED" && (
+                          <div
+                            className={
+                              data.sim.status !== "PENDING"
+                                ? "topup"
+                                : "details"
+                            }
                           >
-                            Top up
-                          </button>
-                        </div>
-                        <div className={data.sim.status === "PENDING" ? "topup" : "details"}>
+                            <button
+                              className="btntopup"
+                              onClick={() => {
+                                setReqObj({
+                                  esimId: data?.esimId,
+                                  planId: data?.plan,
+                                  countryImage: data?.countryImage,
+                                });
+                                setReqObjOpt({
+                                  gb: data?.sim?.size,
+                                  amount: data?.initialPrice,
+                                  country: data.country,
+                                });
+                                setIsViewNetwork(true);
+                              }}
+                              disabled={data.sim.status === "TERMINATED"}
+                            >
+                              Top up
+                            </button>
+                          </div>
+                        )}
+
+                        <div
+                          className={
+                            data.sim.status === "PENDING" ? "topup" : "details"
+                          }
+                        >
                           <button
                             className="btntopup"
                             onClick={() => {
@@ -211,15 +287,26 @@ const Account = ({ setHeaderTitle, parameters, selectedPlan, setActiveButton, se
                               setEsimDetail({});
                               setEsimDetail(data);
                               setActiveButton({
-                                name: data.sim.status === "PENDING" ? "RechargeThePayment" : "PlanDetail",
+                                name:
+                                  data.sim.status === "PENDING"
+                                    ? "RechargeThePayment"
+                                    : "PlanDetail",
                               });
                               if (data.sim.status !== "PENDING") {
-                                localStorage.setItem("qrImage", data.qrCodeImage);
-                                localStorage.setItem("qrString", data.qrCodeString);
+                                localStorage.setItem(
+                                  "qrImage",
+                                  data.qrCodeImage
+                                );
+                                localStorage.setItem(
+                                  "qrString",
+                                  data.qrCodeString
+                                );
                               }
                             }}
                           >
-                            {data.sim.status === "PENDING" ? "Install" : "Details"}
+                            {data.sim.status === "PENDING"
+                              ? "Install"
+                              : "Details"}
                           </button>
                         </div>
                       </div>
