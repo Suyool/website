84a7e05f1c1eee482ab874606e867ae6683e5042
elio najem <elio.najem@elbarid.com>
2024-03-18 12:33:20 +0200

    offers design

--- assets/react/Apps/Simly/Pages/Offers.js
+++ assets/react/Apps/Simly/Pages/Offers.js
@@ -1,73 +1,94 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { settingData, settingObjectData } from "../Redux/Slices/AppSlice";
+import React, {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {settingData, settingObjectData} from "../Redux/Slices/AppSlice";
 import AppAPI from "../Api/AppAPI";
 import SearchBar from "../Component/SearchBar";
 
-const Offers = ({ country }) => {
-  const dispatch = useDispatch();
-  const [searchQuery, setSearchQuery] = useState("");
-  const offre = useSelector((state) => state.appData.offre);
-  const [getOffre,setOffre] = useState([]);
+const Offers = ({country}) => {
+    const dispatch = useDispatch();
+    const [searchQuery, setSearchQuery] = useState("");
+    const offre = useSelector((state) => state.appData.offre);
+    const [filteredOffers, setFilteredOffers] = useState(offre); // Initialize with all offers
 
-  console.log(offre);
-  useEffect(()=>{
-    setOffre(offre);
-    dispatch(
-      settingData({
-        field: "headerData",
-        value: {
-          title: "Global eSIM",
-          backLink: "Packages",
-          currentPage: "Offers",
-        },
-      })
-    );
-},[]);
-  const handleSearchChange = (event) => {
-    const query = event.target.value;
-    setSearchQuery(query);
-  };
+    useEffect(() => {
+        dispatch(
+            settingData({
+                field: "headerData",
+                value: {
+                    title: "Global eSIM",
+                    backLink: "Packages",
+                    currentPage: "Offers",
+                },
+            })
+        );
+    }, []);
+    const handleSearchChange = (event) => {
+        const query = event.target.value.toLowerCase();
+        setSearchQuery(query);
 
-  const handleCardClick = (plan, packages) => {
-    dispatch(
-      settingObjectData({
-        mainField: "headerData",
-        field: "currentPage",
-        value: "PackagesInfo",
-      })
-    );
-    dispatch(
-      settingObjectData({
-        mainField: "simlyData",
-        field: "SelectedPlan",
-        value: plan,
-      })
-    );
-    dispatch(
-      settingObjectData({
-        mainField: "simlyData",
-        field: "SelectedPackage",
-        value: packages,
-      })
-    );
-  };
+        const filteredOffers = offre.filter((item) =>
+            item.planId.toLowerCase().includes(query)
+        );
+        setFilteredOffers(filteredOffers);
+    };
 
-  return (
-    <div className="container itemsPackageCont">
-      <SearchBar
-        searchQuery={searchQuery}
-        handleSearchChange={handleSearchChange}
-        dispatch={dispatch}
-      />
-      <div className="title">Special Offers</div>
-      {getOffre?.map((offreItem, index) => {
-        <>
-        hi
-        </>
-      })}
-    </div>
-  );
+    const handleCardClick = (plan, packages) => {
+        dispatch(
+            settingObjectData({
+                mainField: "headerData",
+                field: "currentPage",
+                value: "PackagesInfo",
+            })
+        );
+        dispatch(
+            settingObjectData({
+                mainField: "simlyData",
+                field: "SelectedPlan",
+                value: plan,
+            })
+        );
+        dispatch(
+            settingObjectData({
+                mainField: "simlyData",
+                field: "SelectedPackage",
+                value: packages,
+            })
+        );
+    };
+
+    return (
+        <div className="container offersCont itemsPackageCont">
+            <SearchBar
+                searchQuery={searchQuery}
+                handleSearchChange={handleSearchChange}
+                dispatch={dispatch}
+            />
+            <div className="title">Special Offers</div>
+            {filteredOffers?.map((item, index) => (
+                <div key={index} className="offerItem">
+                    <div className="card mb-3 bg-package1" onClick={() => handleCardClick(country, packageItem)}>
+                        <div className="card-body">
+                            <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                                <div className="itemsList">
+                                    <h6 className="card-title">
+                                        <img src={item.image} alt={item.planId} width={17}/>
+                                        <span className="ms-2"
+                                              style={{fontFamily: "PoppinsMedium"}}>{item?.planId}</span>
+                                    </h6>
+                                    <p className="card-text itemSize">{item?.size} GB</p>
+                                    <p className="card-text desc">Valid for {item?.duration}</p>
+                                </div>
+                                <div>
+                                    <p className="card-text price">{item?.initial_price_free}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+
 };
 
 export default Offers;
