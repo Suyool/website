ca411dd5c4213f491a4b857e4949dcaf128ac3b7
Anthony Saliba <anthony.saliban@outlook.com>
2024-03-19 14:31:05 +0200

    inventures page

--- src/Controller/MerchantsController.php
+++ src/Controller/MerchantsController.php
@@ -1835,4 +1835,49 @@ class MerchantsController extends AbstractController
 
         return $this->render('interform/index.html.twig', $parameters);
     }
+
+    /**
+     * @Route("/inventures", name="inventures-app")
+     */
+    public function inventures(Request $request, TranslatorInterface $translatorInterface): Response
+    {
+        $parameters = $this->trans->translation($request, $translatorInterface);
+        $translatorInterface->setLocale("en");
+        $parameters['lang'] = "en";
+        $parameters['metaimage'] = "build/images/payroll/inventuresMeta.png";
+        $parameters['descmeta'] = "Why is Suyool the best option for your payroll?";
+        $parameters['faq'] = [
+            "ONE" => [
+                "Title" => "WHAT_IS_SUYOOL_DrogueriePhenicia",
+                "Desc" => "SUYOOL_IS_A_CASHLESS_ECOSYSTEM_THAT_INCORPORATES_DrogueriePhenicia"
+            ],
+            "TWO" => [
+                "Title" => "CAN_ANYONE_OPEN_A_SUYOOL_ACCOUNT_DrogueriePhenicia",
+                "Desc" => "ANY_LEBANESE_CITIZEN_CAN_OPEN_A_SUYOOL_ACCOUNT_DrogueriePhenicia"
+            ],
+            "THREE" => [
+                "Title" => "WHAT_ARE_THE_BENEFITS_FOR_USJ_EMPLOYEES_INVENTURES",
+                "Desc" => "YOU_WILL_BENEFIT_FROM_A_FREE_PLATINUM_MASTERCARD_INVENTURES"
+            ],
+            "FOUR" => [
+                "Title" => "IS_THERE_ANY_FEE_TO_GET_MY_SUYOOL_PLATINUM_MASTERCARD_DrogueriePhenicia",
+                "Desc" => "YOUR_SUYOOL_MASTERCARD_WILL_BE_FREE_OF_CHARGE_ADDICTS_INVENTURES"
+            ],
+            "FIVE" => [
+                "Title" => "WHERE_CAN_I_USE_MY_SUYOOL_PLATINUM_MASTERCARD_DrogueriePhenicia",
+                "Desc" => "YOU_CAN_USE_YOUR_SUYOOL_MASTERCARD_AT_ANY_POS_DrogueriePhenicia"
+            ],
+            "SIX" => [
+                "Title" => "WHERE_CAN_I_WITHDRAW_MY_SALARY_IN_CASH_DrogueriePhenicia_MEDCOPAYROLL",
+                "Desc" => "USERS_CAN_ACCESS_THEIR_MONEY_FROM_MORE_THAN_700_DrogueriePhenicia_MEDCOPAYROLL"
+            ],
+        ];
+
+        $parameters['title'] = "Inventures | Suyool";
+        $parameters['desc'] = "Facing today's financial challenges, we moved our payroll to Suyool. You will get your own digital dual-currency account,a Platinum Debit Card & a payment tool with the best rates available.";
+
+        $parameters['infoSection'] = $this->infoSection2;
+
+        return $this->render('inventures/index.html.twig', $parameters);
+    }
 }
--- translations/messages.en.php
+++ translations/messages.en.php
@@ -559,4 +559,9 @@ return [
     "YOU_WILL_BENEFIT_FROM_A_FREE_PLATINUM_MASTERCARD_INTERFROM" => "They will benefit from a free Visa Platinum debit card the first year & free cash withdrawal of their monthly salary as long as they are an Interform Group employee.",
     "YOUR_SUYOOL_MASTERCARD_WILL_BE_FREE_OF_CHARGE_ADDICTS_INTERFROM" => "Your Suyool Visa Platinum debit card will be free-of-charge as long as you are an Interform Group employee.",
 
+    //Inventures
+    "WHAT_ARE_THE_BENEFITS_FOR_USJ_EMPLOYEES_INVENTURES" => "What are the benefits for Inventures employees?",
+    "YOU_WILL_BENEFIT_FROM_A_FREE_PLATINUM_MASTERCARD_INVENTURES" => "They will benefit from a free Visa Platinum debit card the first year & free cash withdrawal of their monthly salary as long as they are an Inventures employee.",
+    "YOUR_SUYOOL_MASTERCARD_WILL_BE_FREE_OF_CHARGE_ADDICTS_INVENTURES" => "Your Suyool Visa Platinum debit card will be free-of-charge as long as you are an Inventures employee.",
+
 ];
