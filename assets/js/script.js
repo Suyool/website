92a04f17c491fa768a2c6335f32ca2c4d4083b93
elio najem <elio.najem@elbarid.com>
2024-04-03 14:46:16 +0300

    top-section

--- assets/js/script.js
+++ assets/js/script.js
@@ -688,6 +688,34 @@ if(document.getElementById("termsPdfDownloadButton")){
     });
 }
 
+// Set the date we're counting down to
+var countDownDate = new Date("April 20, 2024 10:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("countdownRallyPaper").innerHTML = days + ":" + hours + ":"
+        + minutes + ":" + seconds;
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdownRallyPaper").innerHTML = "EXPIRED";
+    }
+}, 1000);
 // if (document.querySelector(".loaderTopUp")) {
 //   setInterval(function () {
 //     document.getElementById("submitTopUp").style.display = "block";
