e8ab34d4c016b9f76c8f251ed1480e249f592f7f
elio najem <elio.najem@elbarid.com>
2024-04-03 14:46:16 +0300

    top-section

--- src/Controller/AubRallyPaperController.php
+++ src/Controller/AubRallyPaperController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Utils\Helper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AubRallyPaperController extends AbstractController
 {
+    private $hash_algo;
+    private $certificate;
+    private $helper;
+    private $client;
+
+
+    public function __construct()
+    {
+        $this->hash_algo = $_ENV['ALGO'];
+        $this->certificate = $_ENV['CERTIFICATE'];
+        $this->client = HttpClient::create();
+        $this->helper = new Helper($this->client);
+
+    }
 
     /**
      * @Route("/aub-rally-paper", name="app_aub_rally_paper")
@@ -23,28 +38,28 @@ class AubRallyPaperController extends AbstractController
         return $this->render('aubRallyPaper/index.html.twig');
     }
 
-    // /**
-    //  * @Route("/{code}", name="aub_invitation")
-    //  */
-    // public function index($code, Request $request): Response
-    // {
-    //     // $hash = base64_encode(hash($this->hash_algo, $code  . $this->certificate, true));
+     /**
+      * @Route("/{code}", name="aub_invitation")
+      */
+     public function index($code, Request $request): Response
+     {
+//          $hash = base64_encode(hash($this->hash_algo, $code  . $this->certificate, true));
+//
+//         $form_data = [
+//             'Code' => $code,
+//             'Hash' => $hash,
+//         ];
+//
+//         $response = $this->helper->clientRequest($this->METHOD_POST, "{$this->SUYOOL_API_HOST}Utilities/PushUtilityPayment",  $form_data);
 
-    //     $form_data = [
-    //         'Code' => $code,
-    //         'Hash' => $hash,
-    //     ];
-    //     $params['data'] = json_encode($form_data);
-    //     $params['url'] = 'Incentive/CardInvitationDetails';
-    //     $response = Helper::send_curl($params);
-    //     $invitation_card_details_response = json_decode($response, true);
-    //     $invitation_card_details_response = str_replace("{Name}", $invitation_card_details_response['InviterName'], $invitation_card_details_response['RespTitle']);
+//         $invitation_card_details_response = json_decode($response, true);
+//         $invitation_card_details_response = str_replace("{Name}", $invitation_card_details_response['InviterName'], $invitation_card_details_response['RespTitle']);
 
-    //     return $this->render('aubRallyPaper/invitation.html.twig', [
-    //         'inviterDetails' => $invitation_card_details_response,
-    //         'code' => $code,
-    //     ]);
-    // }
+         return $this->render('aubRallyPaper/invitation.html.twig', [
+//             'inviterDetails' => $invitation_card_details_response,
+             'code' => $code,
+         ]);
+     }
 
     /**
      * @Route("/aub-rally-paper-ranking", name="app_aub_rally_paper_ranking")
