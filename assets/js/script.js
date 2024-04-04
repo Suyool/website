c5106bc461cc06d1f63517cde0fb26a618c02431
elio najem <elio.najem@elbarid.com>
2024-04-04 14:42:00 +0300

    login page with db and sessions full flow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+$(document).ready(function() {
+    $('#loginForm').on('submit', function(e) {
+        e.preventDefault();
+        var formData = $(this).serialize();
+        $.ajax({
+            url: '/aub-login',
+            type: 'POST',
+            data: formData,
+            success: function(response) {
+                console.log(response);
+                if(response.flagCode === 2){
+                    $('#loginError').text('Invalid credentials. Please try again.');
+                }else {
+                    window.location.href = '/aub-rally-paper';
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error(xhr.responseText);
+                $('#loginError').text('An error has occurred.');
+            }
+        });
+    });
+});
+
 if(document.getElementById("emailForm")){
 // Get the form element
   const form = document.getElementById("emailForm");
---
+++ src/Entity/AubUsers.php
@@ -0,0 +1,92 @@
+<?php
+// src/Entity/AubUser.php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="aubuser")
+ */
+class AubUsers
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $password; // MD5 hashed password, 32 characters long
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+}
