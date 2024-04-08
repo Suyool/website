d242066aab35a5a7fdd32b66d2dc5973a6ec4d62
unknown <nachoueiry@outlook.com>
2024-04-08 14:03:19 +0300

    Modifying the search bar

--- assets/react/Apps/Simly/Component/Banner.js
+++ assets/react/Apps/Simly/Component/Banner.js
@@ -1,60 +1,56 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {settingObjectData} from "../Redux/Slices/AppSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { settingObjectData } from "../Redux/Slices/AppSlice";
 
-const Banner = ({havingCard}) => {
-    const headerData = useSelector((state) => state.appData.headerData);
-    const parameters = useSelector((state) => state.appData.parameters);
-    const dispatch = useDispatch();
+const Banner = ({ havingCard }) => {
+  const headerData = useSelector((state) => state.appData.headerData);
+  const parameters = useSelector((state) => state.appData.parameters);
+  const dispatch = useDispatch();
 
+  const requestCard = () => {
+    let object = [
+      {
+        exchange: {
+          flag: 93,
+          url: "",
+        },
+      },
+    ];
+    if (parameters?.deviceType === "Android") {
+      window.AndroidInterface.callbackHandler(JSON.stringify(object));
+    } else if (parameters?.deviceType === "Iphone") {
+      window.webkit.messageHandlers.callbackHandler.postMessage(object);
+    }
+  };
 
-
-    const requestCard = () => {
-        let object = [
-            {
-              exchange: {
-                flag: 93,
-                url: "",
-              },
-            },
-          ];
-          if (parameters?.deviceType === "Android") {
-            window.AndroidInterface.callbackHandler(JSON.stringify(object));
-          } else if (parameters?.deviceType === "Iphone") {
-            window.webkit.messageHandlers.callbackHandler.postMessage(object);
-          }
-    };
-
-    const activateEsim = () => {
-        dispatch(
-            settingObjectData({
-                mainField: "headerData",
-                field: "currentPage",
-                value: "Offers",
-            })
-        );
-    };
-
-    return (
-        <div className="banner" style={{textAlign: "center"}}>
-            {havingCard ? (
-                <img
-                    src="build/images/simly/activate1.png"
-                    alt="Activate"
-                    style={{cursor: "pointer",width:"100%"}}
-                    onClick={()=>activateEsim()}
-                />
-            ) : (
-                <img
-                    src="build/images/simly/request1.png"
-                    alt="Request"
-                    onClick={()=>requestCard()}
-                    style={{cursor: "pointer",width:"100%"}}
-                />
-            )}
-        </div>
+  const activateEsim = () => {
+    dispatch(
+      settingObjectData({
+        mainField: "headerData",
+        field: "currentPage",
+        value: "Offers",
+      })
     );
-}
-;
+  };
 
+  return (
+    <div className="banner" style={{ textAlign: "center", display: "none" }}>
+      {havingCard ? (
+        <img
+          src="build/images/simly/activate1.png"
+          alt="Activate"
+          style={{ cursor: "pointer", width: "100%" }}
+          onClick={() => activateEsim()}
+        />
+      ) : (
+        <img
+          src="build/images/simly/request1.png"
+          alt="Request"
+          onClick={() => requestCard()}
+          style={{ cursor: "pointer", width: "100%" }}
+        />
+      )}
+    </div>
+  );
+};
 export default Banner;
--- assets/react/Apps/Simly/Component/SearchBar.js
+++ assets/react/Apps/Simly/Component/SearchBar.js
@@ -1,49 +1,69 @@
 // SearchBar.js
 import React from "react";
-import {settingObjectData} from "../Redux/Slices/AppSlice";
+import { settingObjectData } from "../Redux/Slices/AppSlice";
+import { BiXCircle } from "react-icons/bi";
 
-const SearchBar = ({ searchQuery, handleSearchChange, dispatch }) => {
-    return (
-        <div className="search-bar mt-4">
-            <div className="search-icon-left">
-                <img src="/build/images/g2g/search.svg" alt="Search Icon" />
-            </div>
-            <input
-                type="text"
-                placeholder="Search Destination"
-                value={searchQuery}
-                onChange={handleSearchChange}
-            />
-            <div
-                className="search-icon-right"
-                onClick={() => {
-                    dispatch(
-                        settingObjectData({
-                            mainField: "headerData",
-                            field: "currentPage",
-                            value: "Account",
-                        })
-                    );
-                    dispatch(
-                        settingObjectData({
-                            mainField: "simlyData",
-                            field: "isPackageItem",
-                            value: false,
-                        })
-                    );
-                    dispatch(
-                        settingObjectData({
-                            mainField: "simlyData",
-                            field: "SelectedCountry",
-                            value: null,
-                        })
-                    );
-                }}
-            >
-                <img src="/build/images/topUpSimIcon.svg" alt="Icon" />
-            </div>
-        </div>
-    );
+const SearchBar = ({
+  searchQuery,
+  handleSearchChange,
+  dispatch,
+  setSearchQuery,
+}) => {
+  return (
+    <div className="search-bar mt-4">
+      <div className="search-icon-left">
+        <img src="/build/images/g2g/search.svg" alt="Search Icon" />
+      </div>
+      <div>
+        <input
+          type="search"
+          placeholder="Search Destination"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          //   type="url"
+        />
+        {searchQuery && (
+          <BiXCircle
+            style={{
+              backgroundColor: "#f4f4f4",
+              position: "relative",
+              right: "27px",
+              cursor: "pointer",
+            }}
+            onClick={() => setSearchQuery("")}
+          />
+        )}
+      </div>
+      <div
+        className="search-icon-right"
+        onClick={() => {
+          dispatch(
+            settingObjectData({
+              mainField: "headerData",
+              field: "currentPage",
+              value: "Account",
+            })
+          );
+          dispatch(
+            settingObjectData({
+              mainField: "simlyData",
+              field: "isPackageItem",
+              value: false,
+            })
+          );
+          dispatch(
+            settingObjectData({
+              mainField: "simlyData",
+              field: "SelectedCountry",
+              value: null,
+            })
+          );
+        }}
+      >
+        <img src="/build/images/topUpSimIcon.svg" alt="Icon" />
+      </div>
+    </div>
+  );
 };
 
 export default SearchBar;
